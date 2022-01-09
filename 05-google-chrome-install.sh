74f6a3ccb62793a381c3ccd3df55f96f5fb5deaf
yosjeon at yscart <yosjeon@gmail.com>
2022-01-09 23:25:30 +0900

    Add script to install google chrome browser in terminal

---
+++ 05-google-chrome-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cBlack=$(tput bold)$(tput setaf 0); cRed=$(tput bold)$(tput setaf 1); cGreen=$(tput bold)$(tput setaf 2); cYellow=$(tput bold)$(tput setaf 3); cBlue=$(tput bold)$(tput setaf 4); cMagenta=$(tput bold)$(tput setaf 5); cCyan=$(tput bold)$(tput setaf 6); cWhite=$(tput bold)$(tput setaf 7); cReset=$(tput bold)$(tput sgr0); cUp=$(tput cuu 2)
+
+cat_and_run () {
+	echo "${cYellow}----> ${cGreen}$1 ${cCyan}$2${cReset}"; echo "$1" | sh
+	echo "${cYellow}<${cMagenta}---- ${cBlue}$1 $2${cReset}"
+}
+
+MEMO="구글 크롬 브라우저 설치"
+echo "${cYellow}>>>>>>>>>>${cGreen} $0 ||| ${cCyan}${MEMO} ${cYellow}>>>>>>>>>>${cReset}"
+
+
+cat_and_run "sudo dnf install fedora-workstation-repositories" "3rd Party 저장소 설치"
+cat_and_run "sudo dnf config-manager --set-enabled google-chrome" "Google 크롬 리포지토리를 활성화합니다."
+cat_and_run "sudo dnf install google-chrome-stable" "마지막으로 Chrome을 설치합니다."
+
+
+echo "${cYellow}>>>>>>>>>>${cGreen} $0 ||| ${cCyan}${MEMO} ${cYellow}>>>>>>>>>>${cReset}"
