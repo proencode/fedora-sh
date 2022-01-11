9ef67cebb01c6e8c08f7a077fb183ca2cd59f2bb
yosjeon at fedora <yosjeon@gmail.com>
2022-01-11 15:06:28 +0900

    Added docker & docker-compose install script.

--- 20-docker-and-compose-install.sh
+++ 20-docker-and-compose-install.sh
@@ -34,56 +34,79 @@ fi
 
 # ---
 
-MEMO="Docker-Compose 설치"
+MEMO="Docker 및 Docker-Compose 설치"
 echo "${cYellow}>>>>>>>>>>${cGreen} $0 ||| ${cCyan}${MEMO} ${cYellow}>>>>>>>>>>${cReset}"
 
 # ---
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+cat <<__EOTxt__
+${cBlue}
+cat <<__EOF__
 
-211105
-https://computingforgeeks.com/install-and-use-docker-compose-on-fedora/
+출처: https://computingforgeeks.com/how-to-install-docker-on-fedora/
+Fedora 35/34/33/32/31/30에 Docker CE 설치 2021-11-05
+__EOF__
 
+cat_and_run "sudo dnf -y install dnf-plugins-core" "Fedora 시스템에 저장소를 추가"
 
-docker
-sudo dnf -y install dnf-plugins-core
-sudo tee /etc/yum.repos.d/docker-ce.repo<<EOF
+sudo tee /etc/yum.repos.d/docker-ce.repo <<__EOF__
 [docker-ce-stable]
 name=Docker CE Stable - \$basearch
 baseurl=https://download.docker.com/linux/fedora/35/\$basearch/stable
 enabled=1
 gpgcheck=1
 gpgkey=https://download.docker.com/linux/fedora/gpg
-EOF
-sudo dnf makecache
-sudo dnf install docker-ce docker-ce-cli containerd.io
-sudo systemctl enable --now docker
-systemctl status docker
-sudo usermod -aG docker $(whoami)
-newgrp docker
-docker version
-docker pull alpine
-docker run -it --rm alpine /bin/sh
-/ # apk update
-/ # exit
+__EOF__
+cat_and_run "cat /etc/yum.repos.d/docker-ce.repo" "Fedora 35 에 Docker CE 리포지토리 추가"
 
-sudo dnf install docker-compose
- rpm -qi docker-compose
+cat_and_run "sudo dnf makecache" "저장소 메타 데이터 캐시 생성"
+cat_and_run "sudo dnf install docker-ce docker-ce-cli containerd.io" "Docker CE 설치"
+cat_and_run "sudo systemctl enable --now docker" "도커 서비스 시작 지시"
+cat_and_run "systemctl status docker" "상태 확인"
 
+cat_and_run "sudo usermod -aG docker $(whoami) ; newgrp docker" "docker 그룹에 사용자를 추가"
 
+cat_and_run "docker version" "버전 확인"
+cat_and_run "docker pull alpine" "도커 컨테이너 alpine 설치 테스트"
+cat_and_run "docker run -it --rm alpine /bin/sh" "실행되면, apk update 와 exit 로 테스트합니다."
 
-cat_and_read "sudo apt-get update -y && sudo apt-get install -y docker-ce docker-ce-cli containerd.io"
-cat_and_run "sudo docker -v"
-cat_and_run "sudo systemctl enable docker && sudo service docker start"
-cat_and_run "sudo service docker status"
+cat_and_readY "google-chrome https://computingforgeeks.com/install-docker-ui-manager-portainer/" "도커 UI 관리자 - 포테이너"
+cat_and_readY "google-chrome https://computingforgeeks.com/top-command-for-container-metrics/" "Ctop으로 Docker 컨테이너 리소스 사용량 모니터링"
+${cReset}
+__EOTxt__
 
-cat_and_run "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose"
-cat_and_run "sudo chmod +x /usr/local/bin/docker-compose"
-cat_and_run "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
-cat_and_run "docker-compose -version"
+# ---
+
+cat <<__EOF__
+${cCyan}
+출처: https://computingforgeeks.com/install-docker-ce-on-linux-systems/
+Linux 시스템에 Docker CE를 설치하는 방법 2021-11-08
+${cReset}
+__EOF__
+
+cat_and_read "sudo dnf -y remove docker docker-common docker-selinux docker-engine-selinux docker-engine 2>/dev/null" "이전 버전의 Docker 제거"
+cat_and_run "sudo dnf -y install dnf-plugins-core ; sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo" "Docker 리포지토리 구성:"
+cat_and_run "sudo dnf -y install docker-ce docker-ce-cli containerd.io" "도커 CE 설치"
+cat_and_run "sudo systemctl start docker && sudo systemctl enable docker" "도커 서비스 시작 및 활성화"
+cat_and_run "docker version" "버전 확인"
+
+# ---
+
+cat <<__EOF__
+${cCyan}
+출처: https://computingforgeeks.com/install-and-use-docker-compose-on-fedora/
+Fedora 35/34/33/32/31/30에 Docker CE 설치 2021-11-05
+${cReset}
+__EOF__
+
+cat_and_run "sudo dnf -y install docker-compose" "Fedora 저장소에서 docker-compose 설치"
+cat_and_run "rpm -qi docker-compose" "설치된 내용 확인"
+cat_and_run "sudo curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose ; source /etc/bash_completion.d/docker-compose" "스크립트 넣기"
+
+# ---
+
+cat_and_readY "firefox https://computingforgeeks.com/install-docker-ui-manager-portainer/" "도커 UI 관리자 - 포테이너"
+cat_and_readY "firefox https://computingforgeeks.com/top-command-for-container-metrics/" "Ctop으로 Docker 컨테이너 리소스 사용량 모니터링"
 
 
 echo "${cYellow}>>>>>>>>>>${cGreen} $0 ||| ${cCyan}${MEMO} ${cYellow}>>>>>>>>>>${cReset}"
