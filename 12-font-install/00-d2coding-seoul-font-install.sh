6189953f92fdf536520b6023a0fecc79cf75d110
yo se jeon at vfedora <yosjeon@gmail.com>
2022-03-16 12:43:19 +0900

    스크립트가 있는곳에 압축파일을 둔 것을 전제로 하고, 파일이 있어도 다시 압축해제하도록 하기 위해 -y 옵션을 추가했습니다.

--- 12-font-install/00-d2coding-seoul-font-install.sh
+++ 12-font-install/00-d2coding-seoul-font-install.sh
@@ -7,18 +7,21 @@ cat_and_run () {
 	echo "${cYellow}<${cMagenta}---- ${cBlue}$1 $2${cReset}"
 }
 
-
-# ---
+CMD_NAME=`basename $0` # 명령줄에서 실행 프로그램 이름만 꺼냄
+CMD_DIR=${0%/$CMD_NAME} # 실행 이름을 빼고 나머지 디렉토리만 담음
+if [ "x$CMD_DIR" == "x" ] || [ "x$CMD_DIR" == "x$CMD_NAME" ]; then
+	CMD_DIR="."
+fi
 
 MEMO="한글 폰트파일 설치"
 echo "${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}"
 
-org_dir=$(pwd) #-- 현재의 폴더
+org_dir=$(pwd)/${CMD_DIR}
 font_dir=/usr/share/fonts #-- 폰트 폴더
 
 if [ -f ${org_dir}/Font-D2-KoPub-jeju-nanum-seoul.7z ]; then
 	cat_and_run "ls ${font_dir}" "폰트 등록전의 폴더 내용"
-	cat_and_run "cd ${font_dir} ; sudo 7za x ${org_dir}/Font-D2-KoPub-jeju-nanum-seoul.7z" "폰트 설치"
+	cat_and_run "cd ${font_dir} ; sudo 7za -y x ${org_dir}/Font-D2-KoPub-jeju-nanum-seoul.7z" "폰트 설치"
 	cat_and_run "ls ${font_dir}" "폰트 등록후의 폴더 내용"
 else
 	echo "${cRed}!!!! ${cGreen}----> ${cCyan}${org_dir}/Font-D2-KoPub-jeju-nanum-seoul.7z 폰트 파일이 없습니다.${cReset}"
