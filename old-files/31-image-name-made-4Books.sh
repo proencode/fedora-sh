717d6d2dead1f6d35653aa2416760322c7e8820c
yos at fedora36 <yosjeon@gmail.com>
2022-06-09 17:51:57 +0900

    전에 쓰던 스크립트를 추가합니다.

---
+++ old-files/31-image-name-made-4Books.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [ "x$1" = "x" ]; then
+	#-- (대문자, 공백, - 와 _ 제외한 특수문자) 는 안됨 !
+	image_dir="into_a_mobile_wi-fi_hotspot_img"
+else
+	image_dir="${1}"
+fi
+
+cat <<__EOF__
+----> (대문자, 공백, - 와 _ 제외한 특수문자) 는 안됨 !
+----> Enter image folder name: or just Enter for [ ${image_dir} ]
+__EOF__
+read a
+
+if [ "x$a" != "x" ]; then
+	image_dir="$a"
+fi
+cat <<__EOF__
+.... Image folder name is [ ${image_dir} ]
+__EOF__
+
+enter_string="1.1 Flutter From Zero to Hero LOGO"
+
+until [ "x$enter_string" = "x" ]
+do
+	cat <<__EOF__
+
+${enter_string} ----> 이와 같이 일련번호와 설명을 다음줄에 입력한다.
+__EOF__
+	read enter_string
+	if [ "x$enter_string" != "x" ]; then
+		img_name=$(echo "figure${enter_string,,}" | sed 's/ /_/g') #-- 전부 대문자로 바꾸려면 ${enter_string^^}, 전부 소문자는 ${enter_string,,}
+		cat <<__EOF__
+
+![ Figure${enter_string} ](/${image_dir}/${img_name}.png .webp)
+----> 윗줄을 복사해서 사용한다.
+__EOF__
+	fi
+done
+echo "----> Thank_you for using $0 ~~~"
