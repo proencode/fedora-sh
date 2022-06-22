fe81d276b8ccf099e8833fd1da4efb308c11ce6e
yos at fedora36 <yosjeon@gmail.com>
2022-06-22 16:56:14 +0900

    이미지 이름을 만드는 스크립트를 수정했습니다.

---
+++ 11-markdown-translate-EN-to-KO/medium/2204_springboot_react_3e/aa.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cpto () {
+	echo "----> cp c.md 2204_springboot_react_3e_$1.md"
+	cp c.md 2204_springboot_react_3e_$1.md
+}
+
+cpto 06-104
+cpto 07-105
+cpto 08-200
+cpto 09-206
+cpto 10-207
+cpto 11-208
+cpto 12-209
+cpto 13-300
+cpto 15-311
+cpto 16-312
+cpto 17-313
+cpto 18-314
+cpto 19-315
+cpto 20-316
+cpto 21-400
+cpto 22-500
