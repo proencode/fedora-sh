fe71f4a8a898dc6d8a7e6fe3d7defcd565a6715e
fedora at vfc36 <yosjeon@gmail.com>
2022-07-06 12:22:20 +0900

    git 의 기본설정을 먼저 표시하도록 했습니다.

--- git-pull-ALL-proj-dir.sh
+++ git-pull-ALL-proj-dir.sh
@@ -2,6 +2,14 @@
 
 cBlack=$(tput bold)$(tput setaf 0); cRed=$(tput bold)$(tput setaf 1); cGreen=$(tput bold)$(tput setaf 2); cYellow=$(tput bold)$(tput setaf 3); cBlue=$(tput bold)$(tput setaf 4); cMagenta=$(tput bold)$(tput setaf 5); cCyan=$(tput bold)$(tput setaf 6); cWhite=$(tput bold)$(tput setaf 7); cReset=$(tput bold)$(tput sgr0); cUp=$(tput cuu 2)
 
+cat <<__EOF__
+
+${cYellow}git config --global user.name "${USER} at $(uname -n)"${cReset}
+${cYellow}git config --global user.email "yosjeon@gmail.com"${cReset}
+${cYellow}git config --global alias.ll "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" # ----> 한줄로 로그보기${cReset}
+${cYellow}git config --global --list${cReset}
+__EOF__
+
 cd ~/git-projects
 for dir in $(ls -d *)
 do
