4bd486dde50bbf5106b1f5b28348365ba5677002
fedora at vfc36jj <yosjeon@gmail.com>
2022-08-19 17:43:49 +0900

    수정중입니다.

--- 10-wikijs-docker-by-markdown/db-to-cloud.sh
+++ 10-wikijs-docker-by-markdown/db-to-cloud.sh
@@ -1,9 +1,55 @@
 #!/bin/sh
 
-show_message="NOok" #-- 터미널에서 실행한다면 = "ok" , 메세지를 보여준다.
+CMD_NAME=`basename $0` # 명령줄에서 실행 프로그램 이름만 꺼냄
+CMD_DIR=${0%/$CMD_NAME} # 실행 이름을 빼고 나머지 디렉토리만 담음
+if [ "x$CMD_DIR" == "x" ] || [ "x$CMD_DIR" == "x$CMD_NAME" ]; then
+	CMD_DIR="."
+fi
+source ${CMD_DIR}/color_base #-- cBlack cRed cGreen cYellow cBlue cMagenta cCyan cWhite cReset cUp cat_and_run cat_and_read cat_and_readY
 
-LOGIN_PATH=swlog #-- DB 로그인 패쓰
-DB_NAME=gate242 #-- 백업할 데이터베이스 이름
+#----
+
+show_then_run () {
+	if [ "x$show_ok" = "xok" ]; then cat_and_run "$1" "#-- (${showno}) ${showqq}" ; fi
+}
+show_then_view () {
+	if [ "x$show_ok" = "xok" ]; then echo "${cGreen}----> $1 ${cCyan}#-- (${showno}) ${showqq}${cReset}" ; fi
+}
+show_title () {
+	if [ "x$show_ok" = "xok" ]; then
+		cat <<__EOF__
+    ${cGreen}|
+    |
+    | ${cCyan}$1
+    ${cGreen}|
+    |${cReset}
+__EOF__
+	fi
+}
+#--- value_keyin "LOGIN_PATH" "${LOGIN_PATH}" "데이터베이스의 로그인 패쓰 를 입력하세요."
+value_keyin () {
+	FIELD_NAME=$1
+	FIELD_VALUE=$2
+	FIELD_TITLE=$3
+	cat <<__EOF__
+
+${cGreen}----> ${FIELD_TITLE}[ ${cCyan}${FIELD_VALUE} ${cGreen}]${cReset}
+__EOF__
+	read return_value
+
+	if [ "x$return_value" = "x" ]; then
+		return_value="${FIELD_VALUE}"
+	fi
+	cat <<__EOF__
+${cUp}${cCyan}${FIELD_NAME}: ${cRed} ${cYellow}${return_value} ${cRed}]
+
+__EOF__
+}
+
+#----
+
+show_ok="NOok"
+show_ok="ok" #-- "ok" 라면 터미널에서 실행하는 경우로 보고, 메세지를 보여준다.
 
 this_year=$(date +%Y) #-- 2022
 this_wol=$(date +%m) #-- 07
@@ -13,196 +59,216 @@ week_no=$(date +%u) #------------ 일0 월1 화2 수3 목4 금5 토6
 week_no=$(( ${week_no} + 1 )) #-- 1   2   3   4   5   6   7   #--
 yoil_atog=$(echo "abcdefg" | cut -c ${week_no}) 
 
+if [ "x$1" = "x" ]; then
+	cat <<__EOF__
+
+${cYellow}$1 ${cMagenta}[ DB_NAME ] 을 지정하지 않았으므로 작업을 끝냅니다.${cReset}
+__EOF__
+	exit -1
+fi
+
+if [ "x$1" = "xkaosorder" ]; then
+	DB_NAME="$1" #-- 백업할 데이터베이스 이름
+	LOGIN_PATH="kaoslog" #-- 데이터베이스 로그인 패쓰
+	LOCAL_FOLDER="${HOME}/gate242/${HOSTNAME}" #-- 백업파일을 임시로 저장할 로컬 저장소의 디렉토리 이름
+	REMOTE_FOLDER="gate242db" #-- 원격 저장소의 첫번째 폴더 이름
+	RCLONE_NAME="swlgc" #-- rclone 이름
+else
+if [ "x$1" = "xgate242" ]; then
+	DB_NAME="$1" #-- 백업할 데이터베이스 이름
+	LOGIN_PATH="swlog" #-- 데이터베이스 로그인 패쓰
+	LOCAL_FOLDER="${HOME}/gate242/${HOSTNAME}" #-- 백업파일을 임시로 저장할 로컬 저장소의 디렉토리 이름
+	REMOTE_FOLDER="gate242db" #-- 원격 저장소의 첫번째 폴더 이름
+	RCLONE_NAME="swlgc" #-- rclone 이름
+else
+if [ "x$1" = "xwiki" ]; then
+	DB_NAME="$1" #-- 백업할 데이터베이스 이름
+	LOGIN_PATH="wikipsql" #-- 데이터베이스 로그인 패쓰
+	LOCAL_FOLDER="${HOME}/wiki.js/${HOSTNAME}" #-- 백업파일을 임시로 저장할 로컬 저장소의 디렉토리 이름
+	REMOTE_FOLDER="wiki.js" #-- 원격 저장소의 첫번째 폴더 이름
+	RCLONE_NAME="yosgc" #-- rclone 이름
+else
+fi
+fi
+fi
+
+ENTER_VALUE="NOok" #-- ok=환경변수 대신 하나하나 입력하도록 한다.
+SHOW_OK="NOok"
+if [ "x$2" = "xok" ]; then
+	SHOW_OK="ok"
+else
+	if [ "x$2" = "xenter" ]; then
+		ENTER_VALUE="ok" #-- ok=환경변수 대신 하나하나 입력하도록 한다.
+	else
+		LOGIN_PATH="$2"
+	fi
+fi
+
+if [ "x$1" = "xname" ]; then
+	cat <<__EOF__
+#-- 1		2		3		4		5		6
+#-- DB_NAME	LOGIN_PATH	LOCAL_FOLDER	REMOTE_FOLDER	RCLONE_NAME	SHOW OK?
+#-- kaosorder2	kaosgc		/home/kaosdb	kaosdb		kngc		ok/""
+#-- gate242	swlgc		/home/gate242	gate242		swlgc		ok/""
+#-- wiki	wikipsql	/home/wiki.js	wiki.js		yosgc		ok/""
+#--
+#-- "name" #-- 쓸수 있는 DB_NAME 을 보야주고 끝냅i니다.
+#-- dbname	"" #-- 지정한 데이터베이스로 진행합니다.
+#-- dbname	"ok" #-- 지정한 데이터베이스로 진행하면서 과정을 보여줍니다.
+#-- dbname	"enter" #-- 조건값을 입력 받을수 있도록 한다. 진행 과정도 보여줍니다.
+#-- "" #-- \$1 을 지정하지 않았으므로 바로 종료힙니다.
+__EOF__
+	exit -1
+fi
+
+if [ "x${ENTER_VALUE}" = "xok" ]; then
+	value_keyin "LOGIN_PATH" "${LOGIN_PATH}" "데이터베이스의 로그인 패쓰 를 입력하세요."
+	LOGIN_PATH="${return_value}"
+
+	value_keyin "LOCAL_FOLDER" "${LOCAL_FOLDER}" "백업파일을 임시로 저장할 로컬 저장소의 디렉토리 이름을 입력하세요."
+	LOCAL_FOLDER="${return_value}"
+
+	value_keyin "REMOTE_FOLDER" "${REMOTE_FOLDER}" "원격 저장소의 첫번째 폴더 이름을 입력하세요."
+	REMOTE_FOLDER="${return_value}"
+
+	value_keyin "RCLONE_NAME" "${RCLONE_NAME}" "rclone 이름을 입력하세요."
+	RCLONE_NAME="${return_value}"
+
+DB_NAME
+LOGIN_PATH
+LOCAL_FOLDER	REMOTE_FOLDER	RCLONE_NAME
+SHOW OK?
+fi
+
 uname_n=$(uname -n)
 db_NOWyoil_sql_7z=${DB_NAME}_${ymd_hm}_${uname_n}${yoil_atog}.sql.7z #-- *"[a-g].sql.7z" // 요일 표시
 this_wol_sql_7z="${this_wol}.sql.7z"
 db_nowWOL_sql_7z=${DB_NAME}_${ymd_hm}_${uname_n}.${this_wol_sql_7z} #-- *".07.sql.7z" // 월 표시
 
-LOCAL_DIR=${HOME}/seowontire-data/db_backup/${HOSTNAME}/${this_year}/${this_wol} #-- 백업파일을 임시로 저장할 로컬 저장소의 디렉토리 이름
-REMOTE_YEAR=gate242db/${this_year}
-REMOTE_WOL=${REMOTE_YEAR}/${this_wol} #-- rclone 명령으로 보내는 원격 저장소의 데이터베이스구분/년/월 폴더이름
-rclone_name=swlgc #-- 원격 저장소인 seowontire.libgc 를 연결하기 위해 rclone 에서 쓰는 이름
+LOCAL_THIS_YEAR=${LOCAL_FOLDER}/${this_year} #-- 년도 폴더에는 매월 마지막 백업 1개씩만 보관한다.
+LOCAL_THIS_WOL=${LOCAL_THIS_YEAR}/${this_wol} #-- 년.월별 폴더에는 이달의 마지막 1주일치만 보관한다.
+LOCAL_THIS_WEEK=${LOCAL_THIS_YEAR}/weeks #-- 년도의 weeks 폴더에는 매주 마지막 백업 1개씩만 보관한다.
+
+REMOTE_YEAR=wiki.js/${this_year}
+REMOTE_WOL=${REMOTE_FOLDER}/${this_wol} #-- rclone 명령으로 보내는 원격 저장소의 데이터베이스구분/년/월 폴더이름
+
+rclone_name=yosgc #-- 원격 저장소인 yosjeongc 를 연결하기 위해 rclone 에서 쓰는 이름
 
 
+show_title "${REMOTE_WOL} 월 최근 일주일 백업을 시작니다. (${ymd_hm})"
 
-if [ ! -d ${LOCAL_DIR} ]; then
-	if [ "x$show_message" = "xok" ]; then echo "mkdir -p ${LOCAL_DIR} #-- (1) 보관용 로컬 디렉토리를 만듭니다." ; fi
-	#==
-	mkdir -p ${LOCAL_DIR} #-- (1) 보관용 로컬 디렉토리를 만듭니다.
 
+if [ ! -d ${LOCAL_THIS_WOL} ]; then
+	showno="1a" ; showqq="보관용 로컬 디렉토리를 만듭니다."
+	show_then_run "mkdir -p ${LOCAL_THIS_WOL}"
 else
-	if [ "x$show_message" = "xok" ]; then
-		echo "ls -l ${LOCAL_DIR} #-- (1) 보관용 로컬 디렉토리 입니다."
-		#==
-		ls -l ${LOCAL_DIR} #-- (1) 보관용 로컬 디렉토리 입니다.
-
-	fi
-
+	showno="1b" ; showqq="보관용 로컬 디렉토리 입니다."
+	show_then_run "ls -l ${LOCAL_THIS_WOL}"
 fi
 
 
+showno="2" ; showqq="오늘날짜 클라우드 백업파일이 있는지 확인 합니다."
+show_then_view "REMOTE_SQL_7Z_LIST=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL}/ | grep ${yoil_atog}.sql.7z | awk '{print \$2}')"
+REMOTE_SQL_7Z_LIST=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL}/ | grep ${yoil_atog}.sql.7z | awk '{print $2}')
 
-if [ "x$show_message" = "xok" ]; then
-	echo "#----> (2) 오늘날짜 백업 시작"
-	echo "REMOTE_SQL_7Z_LIST=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL}/ | grep ${yoil_atog}.sql.7z | awk '{print \$2}') #-- (3) 오늘날짜 클라우드 백업파일이 있는지 확인 합니다."
-fi
-#==
-REMOTE_SQL_7Z_LIST=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL}/ | grep ${yoil_atog}.sql.7z | awk '{print $2}') #-- (3) 오늘날짜 클라우드 백업파일이 있는지 확인 합니다.
-
-if [ "x$show_message" = "xok" ]; then echo "REMOTE_SQL_7Z_LIST=----${REMOTE_SQL_7Z_LIST}---- #-- (4)" ; fi
 
 if [ "x$REMOTE_SQL_7Z_LIST" != "x" ]; then
-	if [ "x$show_message" = "xok" ]; then
-		echo "mapfile -t Remote_Sql7z_Array <<< \"\$REMOTE_SQL_7Z_LIST\" #-- (5) Array 만들기"
-       	fi
-	#==
+	showno="3a" ; shwo_msg="클라우드에 오늘날짜 백업파일이 있는 경우,"
+	show_then_view "mapfile -t Remote_Sql7z_Array <<< \"$REMOTE_SQL_7Z_LIST\""
 	mapfile -t Remote_Sql7z_Array <<< "$REMOTE_SQL_7Z_LIST"
 
 	for val in "${Remote_Sql7z_Array[@]}"
 	do
-		file_name=$(echo ${val} | sed 's/ *$//g') #- 빈칸 삭제 // https://linuxhint.com/trim_string_bash/
-		if [ "x$show_message" = "xok" ]; then
-			echo "file_name=\$(echo ${val} | sed 's/ *$//g') #- 빈칸 삭제 // https://linuxhint.com/trim_string_bash/"
-			echo "OUTRC=\$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_WOL}/${file_name}) #-- (6-1) 오늘날짜 클라우드 백업파일을 삭제합니다."
-		fi
-		#==
-		OUTRC=$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_WOL}/${file_name}) #-- (6-1) 오늘날짜 클라우드 백업파일을 삭제합니다.
+		showno="3a1" ; showqq="빈칸 삭제 // https://linuxhint.com/trim_string_bash/"
+		show_then_view "file_name=\$(echo ${val} | sed 's/ *\$//g')"
+		file_name=$(echo ${val} | sed 's/ *$//g')
 
-		if [ "x$show_message" = "xok" ]; then
-			echo "OUTRC=----${OUTRC}----"
-		fi
+		OUTRC=$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_WOL}/${file_name})
+		showno="3a2" ; showqq="오늘날짜 클라우드 백업파일을 삭제합니다."
+		show_then_view "OUTRC=\$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_WOL}/${file_name}) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
+		show_then_view "#"
 	done
 else
-	if [ "x$show_message" = "xok" ]; then echo "#-- (6-2) 클라우드에는 오늘날짜 백업파일이 없습니다." ; fi
+	showno="3b" ; showqq="클라우드에는 오늘날짜 백업파일이 없습니다."
+	show_then_view "#"
 fi
+# echo ">>>> ^C" ; read a
+
+showno="4" ; showqq="오늘날짜 로컬 백업파일을 삭제합니다."
+show_then_run "rm -f ${LOCAL_THIS_WOL}/*${yoil_atog}.sql.7z"
+
+showno="5" ; showqq="DB 를 로컬에 백업합니다."
+show_then_run "/usr/bin/mysqldump --login-path=${LOGIN_PATH} --column-statistics=0 ${DB_NAME} | 7za a -si ${LOCAL_THIS_WOL}/${db_NOWyoil_sql_7z}"
+
+OUTRC=$(/usr/bin/rclone copy ${LOCAL_THIS_WOL}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_WOL}/)
+showno="6" ; showqq="로컬 DB 백업파일을 클라우드로 복사합니다."
+show_then_view "OUTRC=\$(/usr/bin/rclone copy ${LOCAL_THIS_WOL}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_WOL}/) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 
 
-
-if [ "x$show_message" = "xok" ]; then
-	echo "rm -f ${LOCAL_DIR}/*${yoil_atog}.sql.7z #-- (7) *\"[a-g].sql.7z\" 오늘날짜 로컬 백업파일을 삭제합니다."
-fi
-#==
-rm -f ${LOCAL_DIR}/*${yoil_atog}.sql.7z #-- (7) *"[a-g].sql.7z" 오늘날짜 로컬 백업파일을 삭제합니다.
-
-if [ "x$show_message" = "xok" ]; then
-	echo "/usr/bin/mysqldump --login-path=${LOGIN_PATH} --column-statistics=0 ${DB_NAME} | 7za a -si ${LOCAL_DIR}/${db_NOWyoil_sql_7z} #-- (8) DB 를 로컬에 백업합니다."
-fi
-#==
-/usr/bin/mysqldump --login-path=${LOGIN_PATH} --column-statistics=0 ${DB_NAME} | 7za a -si ${LOCAL_DIR}/${db_NOWyoil_sql_7z} #-- (8) DB 를 로컬에 백업합니다.
-
-if [ "x$show_message" = "xok" ]; then
-	echo "OUTRC=\$(/usr/bin/rclone copy ${LOCAL_DIR}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_WOL}/) #-- (9) 로컬 DB 백업파일을 클라우드로 복사합니다."
-fi
-#==
-OUTRC=$(/usr/bin/rclone copy ${LOCAL_DIR}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_WOL}/) #-- (9) 로컬 DB 백업파일을 클라우드로 복사합니다.
-
-if [ "x$show_message" = "xok" ]; then
-	echo "OUTRC=----${OUTRC}----"
-	echo "#<---- (10) 오늘날짜 백업 끝"
-fi
+showno="6" ; showqq="${REMOTE_WOL} 월 최근 일주일 백업을 끝냅니다. (${ymd_hm})"
+show_then_view "#"
 
 
+#--
 
-if [ "x$show_message" = "xok" ]; then
-	echo "#----> (11) 월별 백업 시작"
-	echo "REMOTE_SQL_7Z_LIST=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}/ | grep ${this_wol_sql_7z} | awk '{print \$2}') #-- (12) ${this_wol}월 백업파일이 있는지 확인 합니다."
-fi
-#==
-REMOTE_SQL_7Z_LIST=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}/ | grep ${this_wol_sql_7z} | awk '{print $2}') #-- (12) ${this_wol}월 백업파일이 있는지 확인 합니다.
 
-if [ "x$show_message" = "xok" ]; then echo "REMOTE_SQL_7Z_LIST=----${REMOTE_SQL_7Z_LIST}---- #-- (13)" ; fi
+show_title "${REMOTE_WOL} 월의 마지막날짜로 복사 시작 (${ymd_hm})"
+
+
+showno="11" ; showqq="${this_wol}월 백업파일이 이전에 백업돼 있었는지 확인 합니다."
+show_then_view "REMOTE_SQL_7Z_LIST=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}/ | grep ${this_wol_sql_7z} | awk '{print \$2}')"
+REMOTE_SQL_7Z_LIST=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}/ | grep ${this_wol_sql_7z} | awk '{print $2}')
+
 
 if [ "x$REMOTE_SQL_7Z_LIST" != "x" ]; then
-	if [ "x$show_message" = "xok" ]; then echo "mapfile -t Remote_Sql7z_Array <<< \"\$REMOTE_SQL_7Z_LIST\" #-- (14-1-1) Array 만들기" ; fi
-	#==
+	showno="12a" ; shwo_msg="클라우드에 이달 백업파일이 있는 경우,"
+	show_then_view "mapfile -t Remote_Sql7z_Array <<< \"$REMOTE_SQL_7Z_LIST\""
 	mapfile -t Remote_Sql7z_Array <<< "$REMOTE_SQL_7Z_LIST"
 
 	for val in "${Remote_Sql7z_Array[@]}"
 	do
-		#==
-		file_name=$(echo ${val} | sed 's/ *$//g') #- 빈칸 삭제 // https://linuxhint.com/trim_string_bash/
+		showno="12a1" ; showqq="빈칸 삭제"
+		show_then_view "file_name=\$(echo ${val} | sed 's/ *\$//g')"
+		file_name=$(echo ${val} | sed 's/ *$//g')
 
-		if [ "x$show_message" = "xok" ]; then
-			echo "----------> val ${val};" ;
-			echo "----> file_name ${file_name};"
-			echo "OUTRC=\$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_YEAR}/${file_name}) #-- (14-1-2) ${this_wol}월 백업파일을 삭제합니다."
-		fi
-		#==
-		OUTRC=$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_YEAR}/${file_name}) #-- (14-1-2) ${this_wol}월 백업파일을 삭제합니다.
-
-		if [ "x$show_message" = "xok" ]; then echo "OUTRC=----${OUTRC}----" ; fi
+		OUTRC=$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_YEAR}/${file_name})
+		showno="12a2" ; showqq="${this_wol}월 백업파일을 삭제합니다."
+		show_then_view "OUTRC=\$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_WOL}/${file_name}) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 	done
 else
-	if [ "x$show_message" = "xok" ]; then echo "#-- (14-2) ${this_wol}월 백업파일이 없습니다." ; fi
+	showno="12b" ; showqq="클라우드에는 ${this_wol}월 백업파일이 없습니다."
+	show_then_view "#"
 fi
 
 
 
-if [ "x$show_message" = "xok" ]; then
-	echo "REMOTE_SQL_7Z_LIST=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}/ | grep '.${this_wol}.sql.7z' | awk '{print \$2}') #-- (15) ${this_wol}월 백업입니다."
-fi
-#==
-REMOTE_SQL_7Z_LIST=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}/ | grep '.${this_wol}.sql.7z' | awk '{print $2}') #-- (15) ${this_wol}월 백업입니다.
-
-if [ "x$show_message" = "xok" ]; then echo "REMOTE_SQL_7Z_LIST=----${REMOTE_SQL_7Z_LIST}---- #-- (16)" ; fi
-
-if [ "x$REMOTE_SQL_7Z_LIST" != "x" ]; then
-	if [ "x$show_message" = "xok" ]; then echo "mapfile -t Remote_Sql7z_Array <<< \"$REMOTE_SQL_7Z_LIST\" #-- (17-1-1)" ; fi
-	#==
-	mapfile -t Remote_Sql7z_Array <<< "$REMOTE_SQL_7Z_LIST"
-
-	for val in "${Remote_Sql7z_Array[@]}"
-	do
-		#==
-		file_name=$(echo ${val} | sed 's/ *$//g') #- 빈칸 삭제 // https://linuxhint.com/trim_string_bash/
-
-		if [ "x$show_message" = "xok" ]; then
-			echo "----------> val ${val};"
-			echo "----> file_name ${file_name};"
-			echo "OUTRC=\$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_YEAR}/${file_name}) #-- (17-1-2) ${this_wol}월 백업파일을 삭제합니다."
-		fi
-		#==
-		OUTRC=$(/usr/bin/rclone deletefile ${rclone_name}:${REMOTE_YEAR}/${file_name}) #-- (17-1-2) ${this_wol}월 백업파일을 삭제합니다.
-
-		if [ "x$show_message" = "xok" ]; then echo "OUTRC=----${OUTRC}----" ; fi
-	done
-else
-	if [ "x$show_message" = "xok" ]; then echo "#-- (17-2) ${this_wol}월 백업파일이 없습니다." ; fi
-fi
+showno="13" ; showqq="${REMOTE_WOL} 월 백업을 시작합니다. (${ymd_hm})"
+show_then_view "#"
 
 
-if [ "x$show_message" = "xok" ]; then
-	echo "OUTRC=\$(/usr/bin/rclone copy ${rclone_name}:${REMOTE_WOL}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/) #-- (18) ${this_year}년도 폴더로 복사합니다."
-fi
-#==
-OUTRC=$(/usr/bin/rclone copy ${rclone_name}:${REMOTE_WOL}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/) #-- (18) ${this_year}년도 폴더로 복사합니다.
+OUTRC=$(/usr/bin/rclone copy ${rclone_name}:${REMOTE_WOL}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/)
+showno="14" ; showqq="${this_wol}월 백업파일을 ${this_year}년도 폴더로 복사합니다."
+show_then_view "OUTRC=\$(/usr/bin/rclone copy ${rclone_name}:${REMOTE_WOL}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 
-if [ "x$show_message" = "xok" ]; then
-	echo "OUTRC=----${OUTRC}----"
-	echo "OUTRC=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR})"
-fi
-#==
 OUTRC=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR})
+showno="16" ; showqq="폴더 확인"
+show_then_view "OUTRC=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 
-if [ "x$show_message" = "xok" ]; then
-	echo "OUTRC=----${OUTRC}----"
-	echo "OUTRC=\$(/usr/bin/rclone moveto ${rclone_name}:${REMOTE_YEAR}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/${db_nowWOL_sql_7z}) #-- (19) 파일 이름을 바꿉니다."
-fi
-#==
-OUTRC=$(/usr/bin/rclone moveto ${rclone_name}:${REMOTE_YEAR}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/${db_nowWOL_sql_7z}) #-- (19) 파일 이름을 바꿉니다.
+OUTRC=$(/usr/bin/rclone moveto ${rclone_name}:${REMOTE_YEAR}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/${db_nowWOL_sql_7z})
+showno="18" ; showqq="파일 이름을 바꿉니다."
+show_then_view "OUTRC=\$(/usr/bin/rclone moveto ${rclone_name}:${REMOTE_YEAR}/${db_NOWyoil_sql_7z} ${rclone_name}:${REMOTE_YEAR}/${db_nowWOL_sql_7z}) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 
-if [ "x$show_message" = "xok" ]; then
-	echo "OUTRC=----${OUTRC}----"
-	echo "/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR} # 년도"
-	#==
-	/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}
+OUTRC=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR})
+showno="18" ; showqq="년도"
+show_then_view "OUTRC=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_YEAR}) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
+show_then_view "${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 
-	echo "/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL} # 월"
-	#==
-	/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL}
-fi
+OUTRC=$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL})
+show_then_view "OUTRC=\$(/usr/bin/rclone ls ${rclone_name}:${REMOTE_WOL}) ${cMagenta}#----${cYellow}${OUTRC}${cMagenta}----"
 
-if [ "x$show_message" = "xok" ]; then echo "#<---- (20) 월별 백업 끝" ; fi
+
+showno="19" ; showqq="${REMOTE_WOL} 월 백업을 끝냅니다. (${ymd_hm})"
+show_then_view "#"
 
 
 #|====>>
