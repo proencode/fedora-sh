ef884081683542defa045f45ea3f7faee1d78bd4
yosjeon at yscart <yosjeon@gmail.com>
2022-08-25 14:54:53 +0900

    스크립트 파일을 비교해서 다르면 복사할수 있도록 하는 스크립트입니다.

---
+++ 10-wikijs-docker-by-markdown/05-copyto-gate-kaos-wiki.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+CMD_NAME=`basename $0` # 명령줄에서 실행 프로그램 이름만 꺼냄
+CMD_DIR=${0%/$CMD_NAME} # 실행 이름을 빼고 나머지 디렉토리만 담음
+if [ "x$CMD_DIR" == "x" ] || [ "x$CMD_DIR" == "x$CMD_NAME" ]; then
+	CMD_DIR="."
+fi
+source ${CMD_DIR}/color_base #-- cBlack cRed cGreen cYellow cBlue cMagenta cCyan cWhite cReset cUp cat_and_run cat_and_read cat_and_readY
+
+MEMO="db-to-cloud.sh 비교 및 복사"
+echo "${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}"
+#--xx-- zz00logs_folder="${HOME}/zz00logs" ; if [ ! -d "${zz00logs_folder}" ]; then cat_and_run "mkdir ${zz00logs_folder}" "로그 폴더" ; fi
+#--xx-- zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d-%H%M%S")__RUNNING_${CMD_NAME}" ; touch ${zz00log_name}
+
+
+home_dir=${HOME}/git-projects
+main_dir=${home_dir}/fedora-sh/10-wikijs-docker-by-markdown
+
+for dir in gate242/run_sh kaosorder/run_scripts_dir ubuntu-sh/10-wikijs-docker
+
+do
+	echo "${cGreen}----> ${cYellow}${dir} ${cCyan}디렉토리를 확인합니다.${cReset}"
+	for file in color_base db-to-cloud.sh header
+	do
+		cat_and_run "diff ${main_dir}/${file} ${home_dir}/${dir}/${file}"
+		echo "${cGreen}----> ${cCyan}차이가 있으면 ${cMagenta}^C ${cCyan}로 중단하고, 확인하세요.${cReset}"
+		read a
+	done
+done
+
+
+#--xx-- rm -f ${zz00log_name} ; zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d-%H%M%S")..${CMD_NAME}" ; touch ${zz00log_name}
+#--xx-- cat_and_run "ls --color ${1}" "프로그램들" ; ls --color ${zz00logs_folder}
+echo "${cRed}<<<<<<<<<<${cBlue} $0 ${cRed}||| ${cMagenta}${MEMO} ${cRed}<<<<<<<<<<${cReset}"
