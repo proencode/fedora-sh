a6c7ee235bf8ab5c27eb7f7873b01afd33a6efc7
yos at yosfedora <yosjeon@gmail.com>
2022-09-27 10:56:27 +0900

    source ~/.bashrc 수행 때문에 수정하고 있습니다.

--- 11-markdown-translate-EN-to-KO/HowtoInstallNode.jsOnFedora/001-install-vue-cli.sh
+++ 11-markdown-translate-EN-to-KO/HowtoInstallNode.jsOnFedora/001-install-vue-cli.sh
@@ -5,7 +5,10 @@ MEMO="Vue CLI 설치"
 echo "${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}"
 
 cat_and_run "#-- npm install -g @vue/cli --loglevel verbose" "(0) vue cli 설치 (느린 장비에서 진행 확인하려는 경우)"
+# source ~/.bashrc
 cat_and_run "npm install -g @vue/cli" "(1) vue cli 설치"
+npm install -g @vue/cli ; echo "#-- (1) vue cli 설치"
 cat_and_run "vue --version" "(2) 버전 확인"
+vue --version ; echo "#-- (2) 버전 확인"
 
 echo "${cRed}<<<<<<<<<<${cBlue} $0 ${cRed}||| ${cMagenta}${MEMO} ${cRed}<<<<<<<<<<${cReset}"
