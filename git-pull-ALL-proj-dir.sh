196de4e0f699012a5950a5aa3e1e0f4b3479d6b1
yosj at vFedora37 <yosjeon@gmail.com>
2022-11-18 10:19:18 +0900

    kaos rsync 안내를 추가합니다.

--- git-pull-ALL-proj-dir.sh
+++ git-pull-ALL-proj-dir.sh
@@ -5,6 +5,7 @@ cBlack=$(tput bold)$(tput setaf 0); cRed=$(tput bold)$(tput setaf 1); cGreen=$(t
 cat <<__EOF__
 ssh -oHostKeyAlgorithms=+ssh-dss -Y -p2022 kaoscOKr
 rsync -avzr --delete -e 'ssh -oHostKeyAlgorithms=+ssh-dss -p 2022' --exclude=target/classes kaoscOKr:dir/ ./ #-- 받을때
+rsync -avzr --delete --rsh="/usr/bin/sshpass -f \${HOME}/.ssh/kaosco.4ssh ssh -oHostKeyAlgorithms=+ssh-dss -Y -p2022 -o StrictHostKeyChecking=no -l kaosco" --no-o --no-g --delete kaoscOK:\${from_dir} .
 2. atyosjswlib 격화소양 i6ytjwudrk 촉견폐일소액구 kaosdesysclubi 씨큐둘째프로젝트 3rhg5vyrr
 ${cYellow}git config --global credential.helper store${cReset}
 ${cYellow}git config --global user.password "ATBBn9e5ZGpH6qk8yNbXfMUfeGe49D6AB2" #-- bbop대아투코드네임${cReset}
