8268b5c693b4cd3fb5cde3f38a35dcf056daa517
yosj at vFedora37 <yosjeon@gmail.com>
2022-11-25 11:25:11 +0900

    promise 스크립트에 설명을 추가했습니다.

--- 12-markdown-Archive/exam-js-frontend-backend/03_C2_Exploring_the_Advanced_Concepts_of_JavaScript/promise.js
+++ 12-markdown-Archive/exam-js-frontend-backend/03_C2_Exploring_the_Advanced_Concepts_of_JavaScript/promise.js
@@ -1,24 +1,33 @@
 function wwww(sss, k) {
+	console.log(`02 ----> function wwww(sss, k) { #-- wwww("${sss}", ${k}) 시작.`);
 	return new Promise(
 		function(resolve, reject) {
+			console.log(`05 ----> ====> return new Promise( function(resolve, reject) { #-- 시작.`);
 			var aa="---" + sss + "---";
 			var bb=(k) + 999;
-			console.log(`    aa ${aa};`);
-			console.log(`    bb ${bb} = (k ${k}) + 999;`);
+			console.log(`08 ----- ===== >>>> var aa="${aa}"; <=== "---" + sss + "---"`);
+			console.log(`09 ----- ===== >>>> var bb=${bb}; <=== (k = ${k}) + 999`);
+			console.log(`10 ----- ===== >>>> resolve([aa, bb]); <=== [ "${aa}", ${bb} ]`);
 			resolve([aa, bb]); // triggers the then() method
+			console.log(`12----> ====> return new Promise( function(resolve, reject) { #-- 끝.`);
 		}
 	);
+	console.log(`15 ----> function wwww(sss, k) { #-- 끝.`);
 }
 
-console.log("------------------- Start of Promise");
+console.log("18 ------------------- Start of Promise");
 
 var zz="일이삼사"
 var yy=5
-console.log(`var zz="일이삼사"`);
-console.log(`var yy=5`);
+console.log(`22 var zz="${zz}";`);
+console.log(`23 var yy=${yy};`);
+console.log(`24 wwww(zz,yy).then( #-- wwww("${zz}",${yy}) 호출.`);
 wwww(zz,yy).then(
 	function(txt) {
-		console.log(`End of Promise of txt [0] = ((${txt [0]}))`);
-		console.log(`End of Promise of txt [1] = ((${txt [1]}))`);
+		console.log(`27 ----> function(txt) { #-- 시작.`);
+		console.log(`28 ----> txt [0] = ((${txt [0]}))`);
+		console.log(`29 ----> txt [1] = ((${txt [1]}))`);
+		console.log(`30 ----> function(txt) { #-- 끝.`);
 	}
 );
+console.log(`33 wwww(zz,yy).then( #-- 호출 종료.`);
