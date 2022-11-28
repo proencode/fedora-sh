ba30b8f0828312fab5b11222d1313928cf870e5c
yosj at vFedora37 <yosjeon@gmail.com>
2022-11-28 15:23:04 +0900

    자바스크립트 2장을 마무리합니다.

--- 12-markdown-Archive/exam-js-frontend-backend/05_C3_Getting_Started_with_Vue.js/8316-Using_the_v-model_directive_in_an_input_field.js
+++ 12-markdown-Archive/exam-js-frontend-backend/05_C3_Getting_Started_with_Vue.js/8316-Using_the_v-model_directive_in_an_input_field.js
@@ -1,11 +1,11 @@
-aconst Counter = {
+const Counter = {
   data() {
     return {
       count : 10
     }
   },
+  // === 8316_F-Using_the_v-model_directive_in_an_input_field.js ===
   template : `
-  === 8316_F-Using_the_v-model_directive_in_an_input_field.js ===
     Without v-model:
       <input type="text" :value="count" /> &nbsp;&nbsp;
       count = {{count}} <br><br>
