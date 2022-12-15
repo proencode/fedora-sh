716c1358d0d8c4a0b8db1ca2cc26b11b1408a312
proenpi at pi4b <yosjeon@gmail.com>
2022-12-15 16:35:03 +0900

    git config 명령을 복사하기 좋게 배치를 바꿉니다.

--- git-pull-ALL-proj-dir.sh
+++ git-pull-ALL-proj-dir.sh
@@ -7,13 +7,13 @@ ssh -oHostKeyAlgorithms=+ssh-dss -Y -p2022 kaoscOKr
 rsync -avzr --delete -e 'ssh -oHostKeyAlgorithms=+ssh-dss -p 2022' --exclude=target/classes kaoscOKr:dir/ ./ #-- 받을때
 rsync -avzr --delete --rsh="/usr/bin/sshpass -f \${HOME}/.ssh/kaosco.4ssh ssh -oHostKeyAlgorithms=+ssh-dss -Y -p2022 -o StrictHostKeyChecking=no -l kaosco" --no-o --no-g --delete kaoscOK:\${from_dir} .
 2. atyosjswlib 격화소양 i6ytjwudrk 촉견폐일소액구 kaosdesysclubi 씨큐둘째프로젝트 3rhg5vyrr
-${cYellow}git config --global credential.helper store${cReset}
 ${cYellow}git config --global user.password "ATBBn9e5ZGpH6qk8yNbXfMUfeGe49D6AB2" #-- bbop대아투코드네임${cReset}
+${cYellow}git config --global user.password "ghp_LXVMgJ7YJe16DqPvj4m5Xf26nqqMwh2i0" #--깃헙대이지6신공 221117${cReset}
 ${cYellow}git config --global user.name "${USER} at $(uname -n)" #-- 아이버스${cReset}
 ${cYellow}git config --global user.email "yosjeon@gmail.com" #-- 데몬&${cReset}
-${cYellow}git config --global user.password "ghp_LXVMgJ7YJe16DqPvj4m5Xf26nqqMwh2i0" #--깃헙대이지6신공 221117${cReset}
 ${cYellow}git config --global alias.ll "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" # ----> 한줄로 로그보기${cReset}
 ${cYellow}git config --global --list${cReset}
+${cYellow}git config --global credential.helper store${cReset}
 __EOF__
 
 cd ~/git-projects
