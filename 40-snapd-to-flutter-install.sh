5b582054559176b7434410b3fe9d632e35e4c6f9
yosj at vFedora37 <yosjeon@gmail.com>
2023-01-26 11:09:02 +0900

    flutter 설치 스크립트를 추가합니다.

---
+++ 40-snapd-to-flutter-install.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+source ${HOME}/bin/color_base #-- 221027목-1257 CMD_DIR CMD_NAME cmdRun cmdCont cmdYenter echoSeq 
+MEMO="flutter 설치를 위한 작업들 진행"
+cat <<__EOF__
+${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}
+__EOF__
+# zz00logs_folder="${HOME}/zz00logs" ; if [ ! -d "${zz00logs_folder}" ]; then cmdRun "mkdir ${zz00logs_folder}" "로그 폴더" ; fi
+# zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")__RUNNING_${CMD_NAME}" ; touch ${zz00log_name}
+# ----
+
+
+cmdRun "rpm -qa | grep java | sort | grep java" "(1) 설치돼 있는java 확인"
+cmdYenter "sudo dnf install java-17-openjdk-devel" "(2) java 17 설치"
+cmdRun "sudo dnf install snapd" "(3) snapd 설치"
+cmdRun "sudo ln -s /var/lib/snapd/snap /snap" "(4) /snap 으로 소프트링크 지정"
+cmdRun "sudo snap install hello-world" "(5) snap 확인을 위해 hello-world 를 시험설치"
+cat <<__EOF__
+${cCyan}
+(6) 터미널을 따로 열어서, 아래의 둘중 하나를 실행해서 android-studio 룰 설치해야 합니다.
+
+1) snap 으로 android-studio 를 설치하는 경우: 속도가 느리지만 업그레이드가 편함.
+${cYellow}
+sudo snap install android-studio --classic
+${cCyan}
+또는,
+2) https://developer.android.com/studio 에서 다운로드 받는 경우: 홈에서 다운로드 받은 파일을 지정함.
+${cYellow}
+tar xf  ${cMagenta}~/wind_bada/Downloads/android-studio-2022.1.1.19-linux.tar.gz
+${cGreen}
+----> 설치가 끝나면, ${cRed}'${cYellow}y${cRed}' ${cCyan}를 눌러 주세요.${cReset}
+__EOF__
+read a
+if [ "x$a" != "xy" ]; then
+	exit -1
+fi
+cmdRun "sudo snap install flutter --classic" "(7) flutter 설치"
+cmdRun "flutter sdk-path" "(8) flutter 의 sdk path 확인하거나 다운로드 하기"
+cmdRun "flutter doctor" "(9) flutter 정상작동 확인"
+
+
+# ----
+# rm -f ${zz00log_name} ; zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")..${CMD_NAME}" ; touch ${zz00log_name}
+# ls --color ${zz00logs_folder}
+cat <<__EOF__
+${cRed}<<<<<<<<<<${cBlue} $0 ${cRed}||| ${cMagenta}${MEMO} ${cRed}<<<<<<<<<<${cReset}
+__EOF__
