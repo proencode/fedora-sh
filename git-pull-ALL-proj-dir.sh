bb32beaf1362435b5a4613bf9655bc4a042be535
yosj at vFedora37 <yosjeon@gmail.com>
2023-02-01 10:44:34 +0900

    rclone copy 에서 일부파일만 지정하는 방법을 메모에 추가합니다.

--- git-pull-ALL-proj-dir.sh
+++ git-pull-ALL-proj-dir.sh
@@ -6,6 +6,7 @@ cat <<__EOF__
 ssh -oHostKeyAlgorithms=+ssh-dss -Y -p2022 kaoscOKr
 rsync -avzr --delete -e 'ssh -oHostKeyAlgorithms=+ssh-dss -p 2022' --exclude=target/classes kaoscOKr:dir/ ./ #-- 받을때
 rsync -avzr --delete --rsh="/usr/bin/sshpass -f \${HOME}/.ssh/kaosco.4ssh ssh -oHostKeyAlgorithms=+ssh-dss -Y -p2022 -o StrictHostKeyChecking=no -l kaosco" --no-o --no-g --delete kaoscOK:\${from_dir} .
+rclone copy --include "Ktor*epub" yosjgc:ebooks ~/wind_bada/Downloads/
 2. atyosjswlib 격화소양 i6ytjwudrk 촉견폐일소액구 kaosdesysclubi 씨큐둘째프로젝트 3rhg5vyrr
 ${cYellow}git config --global user.password "ATBBn9e5ZGpH6qk8yNbXfMUfeGe49D6AB2" #-- bbop대아투코드네임${cReset}
 ${cYellow}git config --global user.password "ghp_PNVIuFZ6jot682SkbE8WCBSUQurvjh0c" #--깃헙대1소다나스사 23-3-23목까지${cReset}
