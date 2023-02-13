b92ced8bc446a09abb9833ff6a9de357c16c249b
yosj at fedora <yosjeon@gmail.com>
2023-02-13 10:46:13 +0900

    rclone 에 tpnotemi, tpnote2mi 를 추가하고 about-.sh 스크립트에서 wiki 파라미터를 쓰면 위키 마크다운으로 보여주도록 수정했습니다.

--- bin-original/about-rclone-storage.sh
+++ bin-original/about-rclone-storage.sh
@@ -4,20 +4,27 @@ source ${HOME}/bin/color_base #-- 221027목-1257 CMD_DIR CMD_NAME cmdRun cmdCont
 MEMO="rclone 사이즈 확인"
 cat <<__EOF__
 ${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}
+${cCyan}#--
+#-- 이 스크립트 실행시 "${cYellow}wiki${cCyan}" 파라미터를 추가하면, 결과를 ${cMagenta}wiki.js 마크다운${cCyan}으로 보여줍니다.
+#-- ${cReset}
 __EOF__
 # zz00logs_folder="${HOME}/zz00logs" ; if [ ! -d "${zz00logs_folder}" ]; then cmdRun "mkdir ${zz00logs_folder}" "로그 폴더" ; fi
 # zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")__RUNNING_${CMD_NAME}" ; touch ${zz00log_name}
 # ----
 
 
-cmdRun "rclone about yosjgc:"
-cmdRun "rclone about kaosngc:"
-cmdRun "rclone about swlibgc:"
-cmdRun "rclone about ysj5ncmi:"
-cmdRun "rclone about kaosbmi:"
-cmdRun "rclone about kaosb2mi:"
-cmdRun "rclone about kaosb3mi:"
-cmdRun "rclone about kaosb4mi:"
+if [ "x$1" = "xwiki" ]; then
+	echo "${cRed}----> 완료까지 1분정도 기다려 주세요. <----${cReset}"
+fi
+for hostname in kaosbmi  kaosb2mi  kaosb3mi  kaosb4mi  yosjgc  kaosngc  swlibgc  tpnotemi  tpnote2mi  ysj5ncmi
+do
+	if [ "x$1" = "xwiki" ]; then
+		#-- 이 스크립트 실행시 "wiki" 파라미터를 추가하면, 결과를 wiki.js 마크다운으로 보여준다.
+		cmdRun "rclone about ${hostname}:" "$(date +"%y%m%d%a-%H%M") ${USER}@$(uname -n)" | grep -v "|||" | grep -v "<----" | sed 's/---->/####/'
+	else
+		cmdRun "rclone about ${hostname}:" "$(date +"%y%m%d%a-%H%M") ${USER}@$(uname -n)"
+	fi
+done
 
 
 # ----
