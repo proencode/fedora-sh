648cec1e538a90458483cc825831ece8e0160f0c
yosj at vFed38 <yosjeon@gmail.com>
2023-05-04 13:28:45 +0900

    클라우드간 복사 스크립트를 수정하고 있습니다.

--- bin-original/rclone-size.sh
+++ bin-original/rclone-size.sh
@@ -4,13 +4,14 @@ source ${HOME}/bin/color_base #-- 221027목-1257 CMD_DIR CMD_NAME cmdRun cmdCont
 MEMO="rclone 사이즈 확인"
 cat <<__EOF__
 ${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}
+for cn in jjedone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi ;do echo "----> \$cn:" ;rclone size \$cn: ;done
 __EOF__
 # zz00logs_folder="${HOME}/zz00logs" ; if [ ! -d "${zz00logs_folder}" ]; then cmdRun "mkdir ${zz00logs_folder}" "로그 폴더" ; fi
 # zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")__RUNNING_${CMD_NAME}" ; touch ${zz00log_name}
 # ----
 
 
-for cloud_name in kaos1mi  kaos2mi  kaos3mi  kaos4mi  kaosngc  swlibgc  tpn1mi  tpn2mi  tpn3mi  y5dnmi  y5ncmi  yosjgc  ysw10mi
+for cloud_name in jjedone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi
 do
 	cmdRun "rclone lsd ${cloud_name}: ; rclone size ${cloud_name}:"
 done
---
+++ ls-yosjgc-to-ysw10mi.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "----> rclone moveto yosjgc:Life/ktor/230228-ktor-documentation.7z ysw10mi:from_yosjgc"
+rclone moveto yosjgc:Life/ktor/230228-ktor-documentation.7z ysw10mi:from_yosjgc
+echo "----> rclone moveto yosjgc:Life/movie-mp3/80일간의세계일주1956-1.mp3 ."
+rclone moveto yosjgc:Life/movie-mp3/80일간의세계일주1956-1.mp3 .
+echo "----> rclone moveto yosjgc:Life/movie-mp3/80일간의세계일주1956-2.mp3 ."
+rclone moveto yosjgc:Life/movie-mp3/80일간의세계일주1956-2.mp3 .
+echo "----> rclone moveto yosjgc:Life/Job/01-서원타이어/seowontire-극동원본파일/gate242_211129-150243_g1ssd128.sql.7z swlibgc:from_yosjgc"
+rclone moveto yosjgc:Life/Job/01-서원타이어/seowontire-극동원본파일/gate242_211129-150243_g1ssd128.sql.7z swlibgc:from_yosjgc
+echo "----> rclone moveto yosjgc:sw2021\ 올바로/gate242_220220-184827-alb업로드완료_yscart.sql.7z swlibgc:from_yosjgc"
+rclone moveto yosjgc:sw2021\ 올바로/gate242_220220-184827-alb업로드완료_yscart.sql.7z swlibgc:from_yosjgc
+echo "----> rclone moveto yosjgc:Life/movie-mp3/80일간의세계일주1956-3.mp3 ."
+rclone moveto yosjgc:Life/movie-mp3/80일간의세계일주1956-3.mp3 .
+echo "----> rclone moveto yosjgc:Life/games/EpicInstaller-14.2.1-b0891cd7c2a1457f8da37b9a7c92f820.msi ysw10mi:from_yosjgc"
+rclone moveto yosjgc:Life/games/EpicInstaller-14.2.1-b0891cd7c2a1457f8da37b9a7c92f820.msi ysw10mi:from_yosjgc
+echo "----> rclone moveto yosjgc:root/sms-20211129145547.xml.7z ysw10mi:from_yosjgc"
+rclone moveto yosjgc:root/sms-20211129145547.xml.7z ysw10mi:from_yosjgc
+echo "----> rclone moveto yosjgc:Life/Job/영어카툰/Pro_Git_2판.zip ysw10mi:from_yosjgc"
+rclone moveto yosjgc:Life/Job/영어카툰/Pro_Git_2판.zip ysw10mi:from_yosjgc
+echo "----> rclone moveto yosjgc:root/sms-20211205032452.xml ."
+rclone moveto yosjgc:root/sms-20211205032452.xml .
+echo "----> rclone moveto yosjgc:sms-20211212002643.xml ."
+rclone moveto yosjgc:sms-20211212002643.xml .
+echo "----> rclone moveto yosjgc:sms-20230410144637.xml ."
+rclone moveto yosjgc:sms-20230410144637.xml .
+echo "----> rclone moveto yosjgc:sms-20230416001810.xml ."
+rclone moveto yosjgc:sms-20230416001810.xml .
+echo "----> rclone moveto yosjgc:sms-20230423011118.xml ."
+rclone moveto yosjgc:sms-20230423011118.xml .
+echo "----> rclone moveto yosjgc:sms-20230430013914.xml ."
+rclone moveto yosjgc:sms-20230430013914.xml .
+echo "----> rclone moveto yosjgc:Google\ 포토/2016/2016_12_26_06_42_27.mp4 ysw10mi:from_yosjgc"
+rclone moveto yosjgc:Google\ 포토/2016/2016_12_26_06_42_27.mp4 ysw10mi:from_yosjgc
