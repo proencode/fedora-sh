24b174f47806587f36d81221cca7bbf2c0b83c1f
yosj at vFed38 <yosjeon@gmail.com>
2023-05-15 11:57:57 +0900

    rclone size 를 파일에 저장하는 스크립트를 추가합니다.

--- bin-original/rclone-size.sh
+++ bin-original/rclone-size.sh
@@ -4,7 +4,11 @@ source ${HOME}/bin/color_base #-- 221027목-1257 CMD_DIR CMD_NAME cmdRun cmdCont
 MEMO="rclone 사이즈 확인"
 cat <<__EOF__
 ${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}
+${cBlue}
 for cn in jjedone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi ;do echo "----> \$cn:" ;rclone size \$cn: ;done
+
+sh ~/bin/rclone-size.sh > size-of-rclone-\$(date +%y%m%d-%H%M).txt
+${cReset}
 __EOF__
 # zz00logs_folder="${HOME}/zz00logs" ; if [ ! -d "${zz00logs_folder}" ]; then cmdRun "mkdir ${zz00logs_folder}" "로그 폴더" ; fi
 # zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")__RUNNING_${CMD_NAME}" ; touch ${zz00log_name}
