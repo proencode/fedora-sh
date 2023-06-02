714323cc55fdfab423b014db3c8d0619d1e79d78
yosj at vFed38 <yosjeon@gmail.com>
2023-06-02 16:39:47 +0900

    일지를 마감하고 rclone 확인스크립트를 추가합니다.

---
+++ rclone-all-list/rclone-all-list.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ymd=$(date +%y%m%d-%H%M%S)
+temp_file=qqq_temp-${ymd}
+
+for name in jjedone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi
+do
+	dattim=$(date +%y%m%d%a%H%M%S)
+	echo "----> rclone ls ${name}: > ${ymd}/ls-${name}-${dattim}.list"
+	rclone ls ${name}: > ${temp_file}
+	if [ ! -d ${ymd} ]; then
+		mkdir ${ymd}
+	fi
+	#-- $2 .. $9 혹시 "[0-9] " 가 여러번 나올까봐..
+	awk -v rc_name="$name" -F"[0-9] " '{print $1 " " rc_name ":" $2 $3 $4 $5 $6 $7 $8 $9}' ${temp_file} > ${ymd}/ls-${name}-${dattim}.list
+done
+rm -f ${temp_file}
