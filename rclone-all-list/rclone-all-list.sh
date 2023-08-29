c556b51c3f1d572313fd64c4747db6e09a318773
yosj at vFedora38 <yosjeon@gmail.com>
2023-08-29 14:52:41 +0900

    rclone 리스트 작성 스크립트를 수정합니다.

--- rclone-all-list/rclone-all-list.sh
+++ rclone-all-list/rclone-all-list.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
 ymd=$(date +%y%m%d-%H%M%S)
+if [ ! -d ${ymd} ]; then
+	mkdir ${ymd}
+fi
 temp_file=qqq_temp-${ymd}
+size_file=total_size-${ymd}
 
-for name in edone jjdrb jjone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi
+echo "for cn in edone jjdrb jjone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi yswone ;do echo \"----> \${cn}: \$(rclone size \${cn}: | grep size)\" ; done" > ${ymd}/${size_file}
+
+for CLOUD_NAME in edone jjdrb jjone kaos1mi kaos2mi kaos3mi kaos4mi kaosngc swlibgc tpn1mi tpn2mi tpn3mi y5dnmi y5ncmi yosjgc ysw10mi yswone
 do
 	dattim=$(date +%y%m%d%a%H%M%S)
-	echo "----> rclone ls ${name}: > ${ymd}/ls-${name}-${dattim}.list"
-	rclone ls ${name}: > ${temp_file}
-	if [ ! -d ${ymd} ]; then
-		mkdir ${ymd}
-	fi
+	echo "----> rclone ls ${CLOUD_NAME}: > ${ymd}/ls-${CLOUD_NAME}-${dattim}.list"
+	rclone ls ${CLOUD_NAME}: > ${temp_file}
+	#-- if [ ! -d ${ymd} ]; then
+	#-- 	mkdir ${ymd}
+	#-- fi
 	#-- $2 .. $9 혹시 "[0-9] " 가 여러번 나올까봐..
-	awk -v rc_name="$name" -F"[0-9] " '{print $1 " " rc_name ":" $2 $3 $4 $5 $6 $7 $8 $9}' ${temp_file} > ${ymd}/ls-${name}-${dattim}.list
+	awk -v rc_name="$CLOUD_NAME" -F"[0-9] " '{print $1 " " rc_name ":" $2 $3 $4 $5 $6 $7 $8 $9}' ${temp_file} > ${ymd}/ls-${CLOUD_NAME}-${dattim}.list
+	echo "----> ${CLOUD_NAME}: $(rclone size ${CLOUD_NAME}: | grep size)" >> ${ymd}/${size_file}
 done
 rm -f ${temp_file}
+echo "----> 7za a -mx=9 list-rclone-${ymd}.7z ${ymd} ; ls -l . ${ymd}"
+7za a -mx=9 list-rclone-${ymd}.7z ${ymd} ; ls -l . ${ymd}
