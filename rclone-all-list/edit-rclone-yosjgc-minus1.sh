cf412cb80d5f85042a5b6d1bbe632ca3363ff253
yosj at vFedora38 <yosjeon@gmail.com>
2023-08-31 17:06:37 +0900

    클라우드의 파일 리스트를 만드는 스크립트를 수정합니다.

---
+++ rclone-all-list/edit-rclone-yosjgc-minus1.sh
@@ -0,0 +1,218 @@
+#!/bin/sh
+
+rclone copy yosjgc:"경비및후원.xlsx" .
+
+mkdir -p "Life/"
+cd "Life/"
+echo "Life/"
+rclone copy yosjgc:"Life/2021-08 문서.docx" .
+rclone copy yosjgc:"Life/2021-10 일지.docx" .
+rclone copy yosjgc:"Life/2021-11 공지.docx" .
+cd ../
+
+mkdir -p "Life/2023문서/"
+cd "Life/2023문서/"
+echo "Life/2023문서/"
+rclone copy yosjgc:"Life/2023문서/2021 아파트 관리비.xlsx" .
+rclone copy yosjgc:"Life/2023문서/2021 올바로 02g.docx" .
+rclone copy yosjgc:"Life/2023문서/2021 올바로 02g의 사본.docx" .
+rclone copy yosjgc:"Life/2023문서/2021-11 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2021-12 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2021년 올바로 배출자 ／ 처리자.docx" .
+rclone copy yosjgc:"Life/2023문서/2021년 올바로 배출자 ／ 처리자의 A5 인쇄용.docx" .
+rclone copy yosjgc:"Life/2023문서/2021년 올바로 배출자 ／ 처리자의 사본.docx" .
+rclone copy yosjgc:"Life/2023문서/2021년 올바로 배출자 ／ 처리자의 인쇄본.docx" .
+rclone copy yosjgc:"Life/2023문서/2022 아파트 관리비.xlsx" .
+rclone copy yosjgc:"Life/2023문서/2022-01 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-02 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-03 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-04 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-05 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-06 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-07 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-08 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022-09 일지.docx" .
+rclone copy yosjgc:"Life/2023문서/2022년도 4분기 가계부.xlsx" .
+rclone copy yosjgc:"Life/2023문서/220202 세계 인구자료.xlsx" .
+rclone copy yosjgc:"Life/2023문서/220202 코로나19 실시간 상황판.xlsx" .
+rclone copy yosjgc:"Life/2023문서/A4 컬러 라인 나눔고딕코딩11.docx" .
+rclone copy yosjgc:"Life/2023문서/A5 점선 양식 나눔고딕11.docx" .
+rclone copy yosjgc:"Life/2023문서/Dart test source.docx" .
+rclone copy yosjgc:"Life/2023문서/Doit Flutter 2.0.docx" .
+rclone copy yosjgc:"Life/2023문서/Fedora 가상 시스템 설치.docx" .
+rclone copy yosjgc:"Life/2023문서/Fedora 가상 시스템 인쇄용.docx" .
+rclone copy yosjgc:"Life/2023문서/Flutter에서 MVVM 아키텍처 구현을 위한 초보자 가이드.docx" .
+rclone copy yosjgc:"Life/2023문서/win10 에서 Flutter 설치.docx" .
+rclone copy yosjgc:"Life/2023문서/공유 2021 아파트 관리비.xlsx" .
+rclone copy yosjgc:"Life/2023문서/스프링 부트 + 코틀린.docx" .
+rclone copy yosjgc:"Life/2023문서/얏지 A5 점선 양식.docx" .
+rclone copy yosjgc:"Life/2023문서/영어 공부.docx" .
+rclone copy yosjgc:"Life/2023문서/월말 김어준_index의 사본.xlsx" .
+rclone copy yosjgc:"Life/2023문서/제목 없는 스프레드시트.xlsx" .
+cd ../../
+
+mkdir -p "Life/Job/01-서원타이어/"
+cd "Life/Job/01-서원타이어/"
+echo "Life/Job/01-서원타이어/"
+rclone copy yosjgc:"Life/Job/01-서원타이어/2020-12 On-demand Delivery System with Python & PWA.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/3D CAD 오픈소스 포함 캐드 프로그램.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/Github 사용법.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/Grails 4 튜토리얼.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/gate242 흐름도 A5 양식.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/gate242 흐름도 원본.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/gate242 흐름도.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/xlsTOgukdongdb 이전함수.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/개발일지.docx" .
+cd ../../../
+
+mkdir -p "Life/Job/01-서원타이어/구글 스프레드시트/"
+cd "Life/Job/01-서원타이어/구글 스프레드시트/"
+echo "Life/Job/01-서원타이어/구글 스프레드시트/"
+rclone copy yosjgc:"Life/Job/01-서원타이어/구글 스프레드시트/2019년 캘린더.xlsx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/구글 스프레드시트/구매 주문서.xlsx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/구글 스프레드시트/할 일 목록.xlsx" .
+cd ../../../../
+
+mkdir -p "Life/Job/01-서원타이어/서원자료/"
+cd "Life/Job/01-서원타이어/서원자료/"
+echo "Life/Job/01-서원타이어/서원자료/"
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/2019 올바로 대조작업.xlsx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/교통사고 합의서.xlsx" .
+cd ../../../../
+
+mkdir -p "Life/Job/01-서원타이어/서원자료/설명메모/"
+cd "Life/Job/01-서원타이어/서원자료/설명메모/"
+echo "Life/Job/01-서원타이어/서원자료/설명메모/"
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/200828 폐기물 수탁 재활용 관리대장 출력용 시스템.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/200921 거래내역과 올바로내역의 상호가 틀리는 경우 수정방법.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/220126 올바로만 있음 메뉴에 내역이 나오는 문제 해결 방법.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/220126 인쇄본: 올바로만 있음 메뉴에 내역이 나오는 문제 해결 방법.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/거래처테이블의 여분필드 사용.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/관리대장.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/폐기물 수탁 재활용 관리대장.docx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/설명메모/폐차장 계약 절차.docx" .
+cd ../../../../../
+
+mkdir -p "Life/Job/01-서원타이어/서원자료/올바로 관련자료/2019년도/"
+cd "Life/Job/01-서원타이어/서원자료/올바로 관련자료/2019년도/"
+echo "Life/Job/01-서원타이어/서원자료/올바로 관련자료/2019년도/"
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/올바로 관련자료/2019년도/1901 폐기물 수탁 재활용 관리대장.xlsx" .
+rclone copy yosjgc:"Life/Job/01-서원타이어/서원자료/올바로 관련자료/2019년도/2019 올바로등록.docx" .
+cd ../../../../../../
+
+mkdir -p "Life/Job/01-서원타이어/"
+cd "Life/Job/01-서원타이어/"
+echo "Life/Job/01-서원타이어/"
+rclone copy yosjgc:"Life/Job/01-서원타이어/제목 없는 문서.docx" .
+rclone copy yosjgc:"Life/Job/02-카오스패션/2021-5 카오스노트.docx" .
+rclone copy yosjgc:"Life/Job/02-카오스패션/Kaossam 샘플실 납품외주관리.xlsx" .
+cd ../../../
+
+cd "Life/Job/메디/"
+echo "Life/Job/메디/"
+rclone copy yosjgc:"Life/Job/메디/[xls] 품목별 수량 합계 만들기.xlsx" .
+cd ../../../
+
+mkdir -p "Life/Job/프로그램/"
+cd "Life/Job/프로그램/"
+echo "Life/Job/프로그램/"
+rclone copy yosjgc:"Life/Job/프로그램/Grails 4 SSC 예제.docx" .
+cd ../../../
+
+mkdir -p "Life/정치/"
+cd "Life/정치/"
+echo "Life/정치/"
+rclone copy yosjgc:"Life/정치/더민주명단.xlsx" .
+cd ../../
+
+cd "Life/책메모/노자/"
+echo "Life/책메모/노자/"
+rclone copy yosjgc:"Life/책메모/노자/이경숙 노자도덕경.docx" .
+cd ../../../
+
+mkdir -p "Life/책메모/채근담/"
+cd "Life/책메모/채근담/"
+echo "Life/책메모/채근담/"
+rclone copy yosjgc:"Life/책메모/채근담/채근담 전집-100 완역.docx" .
+rclone copy yosjgc:"Life/책메모/채근담/채근담 전집-225 원본.docx" .
+rclone copy yosjgc:"Life/책메모/채근담/채근담 후집-134 완역.docx" .
+rclone copy yosjgc:"Life/책메모/채근담/채근담 후집-134 원본.docx" .
+cd ../../../
+
+mkdir "Life/책메모/"
+cd "Life/책메모/"
+echo "Life/책메모/"
+rclone copy yosjgc:"Life/책메모/춘향전(春香傳) 3.docx" .
+cd ../../
+
+mkdir -p "Life/현대 메모/"
+cd "Life/현대 메모/"
+echo "Life/현대 메모/"
+rclone copy yosjgc:"Life/현대 메모/2020 아파트 관리비.xlsx" .
+rclone copy yosjgc:"Life/현대 메모/2020년 일지.docx" .
+rclone copy yosjgc:"Life/현대 메모/2021 개발일지.docx" .
+rclone copy yosjgc:"Life/현대 메모/2021년 일지.docx" .
+rclone copy yosjgc:"Life/현대 메모/Cm 을 픽셀로 바꾸기.xlsx" .
+rclone copy yosjgc:"Life/현대 메모/냉수를 졸졸 흐르게 한 것임.xlsx" .
+rclone copy yosjgc:"Life/현대 메모/통장관리.xlsx" .
+cd ../../
+
+mkdir -p "Life/현대 메모/2020사진/"
+cd "Life/현대 메모/2020사진/"
+echo "Life/현대 메모/2020사진/"
+rclone copy yosjgc:"Life/현대 메모/2020사진/2020 가계부.xlsx" .
+rclone copy yosjgc:"Life/현대 메모/2020사진/2020 아파트 관리비 공유.xlsx" .
+rclone copy yosjgc:"Life/현대 메모/2020사진/2021 간편 달력.docx" .
+rclone copy yosjgc:"Life/현대 메모/2020사진/2021 아파트 관리비 공유.xlsx" .
+rclone copy yosjgc:"Life/현대 메모/2020사진/간편 가계부.xlsx" .
+cd ../../../
+
+mkdir -p "root/"
+cd "root/"
+echo "root/"
+rclone copy yosjgc:"root/Google 노트에서 가져온 문서 - 내 노트.docx" .
+rclone copy yosjgc:"root/Google 노트에서 가져온 문서 - 노트 1.docx" .
+rclone copy yosjgc:"root/서신.docx" .
+rclone copy yosjgc:"root/진접현대일지.docx" .
+rclone copy yosjgc:"root/프로젝트 제안서.docx" .
+cd ../
+
+mkdir -p "root/old현대/"
+cd "root/old현대/"
+echo "root/old현대/"
+rclone copy yosjgc:"root/old현대/무선 네트워크 없이 hp 프린터 스마트폰 인쇄.docx" .
+rclone copy yosjgc:"root/old현대/카톡 백업하기.docx" .
+rclone copy yosjgc:"root/old현대/핸드폰 앱 정리.xlsx" .
+cd ../../
+
+mkdir -p "root/old현대/보관/"
+cd "root/old현대/보관/"
+echo "root/old현대/보관/"
+rclone copy yosjgc:"root/old현대/보관/2020 아파트 관리비의 사본.xlsx" .
+rclone copy yosjgc:"root/old현대/보관/82쿡 대구언니야들아, 내가 비밀한 개 알려주까.docx" .
+rclone copy yosjgc:"root/old현대/보관/스미싱 사칭문자 사기.docx" .
+cd ../../../
+
+mkdir -p "현대 공유/"
+cd "현대 공유/"
+echo "현대 공유/"
+rclone copy yosjgc:"현대 공유/2021 고정비 지출.xlsx" .
+rclone copy yosjgc:"현대 공유/2021년 가계부.xlsx" .
+rclone copy yosjgc:"현대 공유/2022년 가계부.xlsx" .
+rclone copy yosjgc:"현대 공유/2023년 가계부.xlsx" .
+rclone copy yosjgc:"현대 공유/5 tiny habits for a happy life.docx" .
+rclone copy yosjgc:"현대 공유/Grails 사용.docx" .
+rclone copy yosjgc:"현대 공유/VS Code 강의 번역.docx" .
+rclone copy yosjgc:"현대 공유/박셩리선생 일본산고.docx" .
+rclone copy yosjgc:"현대 공유/심심할때 보는 유튜브.docx" .
+rclone copy yosjgc:"현대 공유/행복한 삶을 위한 다섯가지 작은 습관.docx" .
+rclone copy yosjgc:"현대 공유/현대 게시판.docx" .
+cd ../
+
+mkdir -p "현대 공유/Old files/"
+cd "현대 공유/Old files/"
+echo "현대 공유/Old files/"
+rclone copy yosjgc:"현대 공유/Old files/2020 고정비 지출.xlsx" .
+rclone copy yosjgc:"현대 공유/Old files/2021-1 2 3 가계부.xlsx" .
+cd ../../
+
