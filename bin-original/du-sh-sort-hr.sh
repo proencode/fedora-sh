5fdcbe360759e9bab1dd8541ec2cdfb7aaf5cd57
yosjeon at yscart <yosjeon@gmail.com>
2023-09-10 12:18:56 +0900

    du-sh 스크립트를 수정하고 2309 일지를 변경합니다.

--- bin-original/du-sh-sort-hr.sh
+++ bin-original/du-sh-sort-hr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-source ${HOME}/bin/color_base #-- 221027목-1257 CMD_DIR CMD_NAME cmdRun cmdCont cmdYenter echoSeq 
-# MEMO="디렉토리의 파일별 사이즈를 표시"
+hhh=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2) #-- 230908
+
 # cat <<__EOF__
 # ${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cReset}
 # __EOF__
@@ -9,37 +9,71 @@ source ${HOME}/bin/color_base #-- 221027목-1257 CMD_DIR CMD_NAME cmdRun cmdCont
 # zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")__RUNNING_${CMD_NAME}" ; touch ${zz00log_name}
 # ----
 
-zz00logs=zz00logs
-if [ ! -d "${zz00logs}" ]; then
-	cmdRun "mkdir -p ${zz00logs}" "로그를 담는 디렉토리를 만듭니다."
-fi
-fname=zz.du-sh-sort-hr
 if [ "x$1" = "x" ]; then
-	cnt=15
+	curr_cnt=15
 else
-	cnt=$1
-	if [ "x$2" != "x" ]; then
-		fname=$2
-	fi
+	curr_cnt=$1
 fi
+
+echo "----> 표시할 줄수: [${curr_cnt}]"
+read cnt ; echo "${uuu}"
+if [ "x${cnt}" = "x" ]; then
+	cnt=${curr_cnt}
+fi
+echo "${rrr}[ ${yyy}${cnt} ${rrr}]${xxx}"
+
+if [ "x$2" = "x" ]; then
+	curr_dir="$(pwd)"
+else
+	curr_dir="$2"
+fi
+
+echo "----> 조사하는 디렉토리: [${curr_dir}]"
+read search_dir ; echo "${uuu}"
+if [ "x${search_dir}" = "x" ]; then
+	search_dir=${curr_dir}
+fi
+echo "${rrr}[ ${yyy}${search_dir} ${rrr}]${xxx}"
+
+if [ "x$3" = "x" ]; then
+	curr_savelog="/home/${USER}/zz00logs"
+else
+	curr_savelog="$3"
+fi
+
+echo "----> 로그 담는 디렉토리: [${curr_savelog}]"
+read log_save ; echo "${uuu}"
+if [ "x${log_save}" = "x" ]; then
+	log_save=${curr_savelog}
+fi
+echo "${rrr}[ ${yyy}${log_save} ${rrr}]${xxx}"
+
+if [ ! -d "${log_save}" ]; then
+	echo "mkdir -p ${log_save} #-- 로그를 담는 디렉토리를 만듭니다."
+	mkdir -p ${log_save} #-- 로그를 담는 디렉토리를 만듭니다."
+fi
+
 now=$(date +"%H:%M:%S")
-fDirName=${zz00logs}/${fname}.$(date +"%y%m%d%a-%H%M%S")
+du_sh_sort_hr_file=${log_save}/zz.$(date +"%y%m%d%a-%H%M%S")
 
 
-MEMO="[줄수=${cnt}] [파일명=${fDirName}]"
+MEMO="[줄수=${cnt}] [시작위치=${search_dir}] [파일명=${du_sh_sort_hr_file}]"
+
 cat <<__EOF__
-${cMagenta}>>>>>>>>>>${cGreen} $0 ${cMagenta}||| ${cCyan}${MEMO} ${cMagenta}>>>>>>>>>>${cBlue}
+${mmm}>>>>>>>>>>${ggg} $0 ${mmm}||| ${ccc}${MEMO} ${mmm}>>>>>>>>>>${bbb}
 __EOF__
-ls ${zz00logs}
+ls ${log_save}
 
 
-sudo du -sh * .??* | sort -hr > ${fDirName}
-echo -e "${cReset}$(head -${cnt} ${fDirName})"
+echo "----> cd ${search_dir} ; sudo du -sh * .??* | sort -hr > ${du_sh_sort_hr_file}"
+cd ${search_dir} ; sudo du -sh * .??* | sort -hr > ${du_sh_sort_hr_file}
+echo "----> echo -e \"\${xxx}\$(head -\${cnt} \${du_sh_sort_hr_file})"
+echo -e "${xxx}$(head -${cnt} ${du_sh_sort_hr_file})"
 
 
 # ----
 # rm -f ${zz00log_name} ; zz00log_name="${zz00logs_folder}/zz.$(date +"%y%m%d%a-%H%M%S")..${CMD_NAME}" ; touch ${zz00log_name}
 # ls --color ${zz00logs_folder}
 cat <<__EOF__
-${cRed}<<<<<<<<<<${cBlue} $0 ${cRed}||| ${cMagenta}${MEMO} ${cRed}<<<<<<<<<<${cReset}
+${rrr}<<<<<<<<<<${bbb} $0 ${rrr}||| ${mmm}${MEMO} ${rrr}<<<<<<<<<<${xxx}
 __EOF__
