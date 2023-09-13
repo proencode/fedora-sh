24b7b9a7ffce62d7c07cff4fdb6392281d646e3b
yosjeon at yscart <yosjeon@gmail.com>
2023-09-13 16:10:26 +0900

    kaos 의 base 년도별 압축파일을 만듭니다.

---
+++ 15-base-to-zip.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+for base in cadbase emailbase georaebase scanbase
+do
+	for year in 2010  2011  2012  2013  2014  2015  2016  2017  2018  2019  2020  2021  2022  2023
+	do
+		for month in {1..12}
+		do
+			a="00${month}"
+			mm=${a: -2}
+			base_year_mm=${base}/${year}/${mm}
+			if [ -d ${base_year_mm} ]; then
+				psco="zkdhtm${year}"
+				echo "$(date +'%H:%M:%S:')#----> ls -alR ${base_year_mm} > ${base}/${year}/${base}-${year}-${mm}.ls-alR" >> zip-log
+				ls -alR ${base_year_mm} > ${base}/${year}/${base}-${year}-${mm}.ls-alR
+				echo "$(date +'%H:%M:%S:')#----> 7za a -mx=9 -p${psco} ${base}/${year}/${base}-${year}-${mm}.7z ${base}/${year}/${mm}" >> zip-log
+				7za a -mx=9 -p${psco} ${base}/${year}/${base}-${year}-${mm}.7z ${base}/${year}/${mm}
+			fi
+		done
+	done
+done
