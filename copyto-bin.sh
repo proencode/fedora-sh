5462d2f988e18cb9516977164fe9480b67fc625e
yosj at gusudong <yosjeon@gmail.com>
2024-01-02 13:06:37 +0900

    ~/bin 복사후 확인하도록 수정합니다.

--- copyto-bin.sh
+++ copyto-bin.sh
@@ -6,3 +6,5 @@ echo "----> cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/"
 cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/
 echo "----> cp ~/git-projects/fedora-sh/rclone-all-list/[ilr]* ~/bin/"
 cp ~/git-projects/fedora-sh/rclone-all-list/[ilr]* ~/bin/
+echo "----> ls -al ~/bin"
+ls -al ~/bin
