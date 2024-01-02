fbe67aaeea9e5e2e6583ce094ffcdf3998ab7606
yosj at gusudong <yosjeon@gmail.com>
2024-01-02 13:03:23 +0900

    홈에 bin 디렉토리를 만들고 스크립트를 복사하는 스크립트를 수정합니다.

--- copyto-bin.sh
+++ copyto-bin.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "----> cp bin-original/du-sh-sort-hr.sh ~/bin/"
-cp bin-original/du-sh-sort-hr.sh ~/bin/
-echo "----> rclone-all-list/ifconfig-192.168.sh ~/bin/"
-rclone-all-list/ifconfig-192.168.sh ~/bin/
-echo "----> rclone-all-list/rclone-all-list.sh ~/bin/"
-rclone-all-list/rclone-all-list.sh ~/bin/
+echo "----> mkdir ~/bin"
+mkdir ~/bin
+echo "----> cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/"
+cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/
+echo "----> cp ~/git-projects/fedora-sh/rclone-all-list/[ilr]* ~/bin/"
+cp ~/git-projects/fedora-sh/rclone-all-list/[ilr]* ~/bin/
