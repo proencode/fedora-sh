e4befc515bfa3b677dbbd498ad3bd2f914171c3f
proenpi at pi4b <yosjeon@gmail.com>
2024-01-08 15:31:46 +0900

    78-당월 서버 로컬 클라우드 백업을 확인하고 ping 확인 스크립트를 추가합니다.

--- bin-original/78-kaos-this-month-data.sh
+++ bin-original/78-kaos-this-month-data.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-ssh -oHostKeyAlgorithms=+ssh-dss -Y -p 2022 kaosco@kaos.kr ls -l /var/base/*base/$(date +%Y)/$(date +%m)/ /var/base_db/kaosorder2/$(date +%Y)/$(date +%m)/
+y4=$(date +%Y)
+y2=${y4:2:2}
+m2=$(date +%m)
+
+echo "----> 당월의 서버 확인: ssh -oHostKeyAlgorithms=+ssh-dss -Y -p 2022 kaosco@kaos.kr ls -l /var/base/*base/${y4}/${m2}/ /var/base_db/kaosorder2/${y4}/${m2}/"
+ssh -oHostKeyAlgorithms=+ssh-dss -Y -p 2022 kaosco@kaos.kr ls -l /var/base/*base/${y4}/${m2}/ /var/base_db/kaosorder2/${y4}/${m2}/
+
+echo "----> 당월의 백업 디렉토리 확인: ls -l /opt/backup/kaosdb/${y4}/*/${m2}/"
+ls -l /opt/backup/kaosdb/${y4}/*/${m2}/
+
+for cegs_base in cadbase emailbase georaebase scanbase
+do
+	echo "----> 클라우드 백업: rclone ls kaosngc:kaosdb/${y4}/${cegs_base}/${m2}"
+	rclone ls kaosngc:kaosdb/${y4}/${cegs_base}/${m2}
+done
+echo "----> 클라우드 백업: rclone ls kaosngc:kaosdb/${y4}/kaosorder2/${m2}"
+rclone ls kaosngc:kaosdb/${y4}/kaosorder2/${m2}
