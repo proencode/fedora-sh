13f39edfcf0b04bc26b553d49e11a515d6fdbe46
orangepi at orangepizero2w <yosjeon@gmail.com>
2024-02-03 12:12:24 +0900

    rclone ls 에서 프롬프트 에색상을 추가합니다.

--- rclone-all-list/yesterday-rclone-lsl.sh
+++ rclone-all-list/yesterday-rclone-lsl.sh
@@ -3,7 +3,7 @@
 lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
 
 day_ago=""
-echo "----> [Enter] 또는 0=오늘($(date +%y%m%d_%a)), 1=어제($(date -d "1 day ago" +%y%m%d_%a)), 2=그제($(date -d "2 day ago" +%y%m%d_%a)), ..."
+echo "${ccc}----> ${bbb}[${yyy}Enter${bbb}] 또는 [${yyy}0${bbb}]=오늘(${rrr}$(date +%y%m%d_%a)${bbb}), [${yyy}1${bbb}]=어제(${rrr}$(date -d "1 day ago" +%y%m%d_%a)${bbb}), [${yyy}2${bbb}]=그제(${rrr}$(date -d "2 day ago" +%y%m%d_%a)${bbb}), ... [${yyy}31${bbb}]=(${rrr}$(date -d "31 day ago" +%y%m%d_%a)${bbb})${xxx}"
 read a
 if [ "x$a" = "x" ] || [ $a -lt 1 ]; then
 	grep_y2mmdd=$(date +%y%m%d)
