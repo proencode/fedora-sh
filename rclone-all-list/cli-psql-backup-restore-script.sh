c3261227bc4b752c172d6d2eddef5f6268a2f870
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-02-13 17:08:19 +0900

    백업 스크립트 변수저장 부분을 수정합니다.

--- rclone-all-list/cli-psql-backup-restore-script.sh
+++ rclone-all-list/cli-psql-backup-restore-script.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
 cat <<__EOF__
-#-- wiki58 240213-1443
+#-- wiki58 240213-1703
 
 proenpi@pi4b /opt/backup/wikidb $
 yoran@orangepizero2w /home/docker/backup $
 
 __EOF__
 
+DB_NAME="wikidb" #-- pgsql 데이터베이스 이름 #--> 정리한 이름들
+DB_USER="imwiki" #-- pgsql 사용자 이름
+DB_PASS="wikiam9ho" #-- pgsql 비번
+DB_VOLUME="/home/docker/pgsqlwiki" #-- DB 설치위치
+DB_CONTAINER="dbcont" #-- 데이터베이스 컨테이너
+#--
+WIKI_PORTS="5900" #-- ports: 5900
+WIKI_CONTAINER="wikicont" #-- 위키 컨테이너 #<-- 정리한 이름들
+
 DB_NAME="wiki" #-- DB_NAME: wiki > wikidb #-- pgsql 데이터베이스 이름
 DB_USER="wikijs" #-- DB_USER: wikijs > imwiki #-- pgsql 사용자 이름
-DB_VOLUME="/home/docker/pgsqlwiki" #-- db: volumes: /home/dicker/pgsqlwiki #-- DB 설치위치
-DB_CONTAINER="wikijsdb" #-- db: container_name: wikijsdb > dncont #-- 데이터베이스 컨테이너
+DB_PASS="wikijsrocks" #-- DB_PASS: wikijsrocks > wikiam9ho #-- pgsql 비번
+DB_VOLUME="/home/docker/pgsqlwiki" #-- db: volumes: /home/docker/pgsqlwiki #-- DB 설치위치
+DB_CONTAINER="wikijsdb" #-- db: container_name: wikijsdb > dbcont #-- 데이터베이스 컨테이너
 #--
 WIKI_PORTS="5900" #-- ports: 5900
 WIKI_CONTAINER="wikijs" #-- wiki: container_name: wikijs > wikicont #-- 위키 컨테이너
@@ -88,10 +98,10 @@ else
     echo "----> sudo docker exec -it ${DB_CONTAINER} createdb -U ${DB_USER} ${DB_NAME}"
     sudo docker exec -it ${DB_CONTAINER} createdb -U ${DB_USER} ${DB_NAME}
     cat <<__EOF__
-----> time 7za x -so ${RESTORE_FILE} | sudo docker exec -i pgsql psql -U ${DB_USER} postgress
+----> time 7za x -so ${RESTORE_FILE} | sudo docker exec -i ${DB_CONTAINER} psql -U ${DB_USER} postgress
 ----> 백업했을때, 저 파일에 지정한 비번을 입력하세요.
 __EOF__
-    time 7za x -so ${RESTORE_FILE} | sudo docker exec -i pgsql psql -U ${DB_USER} postgres
+    time 7za x -so ${RESTORE_FILE} | sudo docker exec -i ${DB_CONTAINER} psql -U ${DB_USER} postgres
 cat <<__EOF__
 ----> 리스토어 하기전 최종 백업한 BACKUP_FILE: ${BACKUP_FILE}
 $(ls -l ${BACKUP_FILE})
@@ -108,10 +118,10 @@ services:
   db:
     image: postgres:11-alpine
     environment:
-      POSTGRES_DB: ${DB_NAME} #-- DB_NAME: wiki > wikidb #-- pgsql 데이터베이스 이름
+      POSTGRES_DB: ${DB_NAME} #-- DB_NAME: wiki > wikidb #-- 데이터베이스 이름
 #--
-      POSTGRES_USER: ${DB_USER} #-- DB_USER: wikijs > imwiki #-- pgsql 사용자 이름
-      POSTGRES_PASSWORD: wikijsrocks #-- wikiam9ho #-- wikijsrocks 비번
+      POSTGRES_USER: ${DB_USER} #-- DB_USER: wikijs > imwiki #-- 데이터베이스 사용자 이름
+      POSTGRES_PASSWORD: ${DB_PASS} #-- DB_PASS: wikijsrocks > wikiam9ho #-- 데이터베이스 비번
 
     logging:
       driver: "none"
@@ -131,7 +141,7 @@ services:
       DB_PORT: 5432
       DB_NAME: ${DB_NAME} #-- wiki > wikidb #-- wiki 데이터베이스 이름
       DB_USER: ${DB_USER} #-- wikijs > imwiki #-- wikijs 사용자 이름
-      DB_PASS: wikijsrocks #-- wikiam9ho #-- wikijsrocks 비번
+      DB_PASS: ${DB_PASS} #-- wikijsrocks > wikiam9ho #-- wikijsrocks 비번
 
     restart: unless-stopped
     ports:
