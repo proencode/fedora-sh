0215f2256ed5dfea28725581593e62d88e735864
yosj at u2204svr <yosjeon@gmail.com>
2024-02-15 12:22:30 +0900

    bin 으로 복사하는 스크립트에 y 를 추가합니다.

--- copyto-bin.sh
+++ copyto-bin.sh
@@ -6,7 +6,7 @@ if [ ! -d ~/bin ]; then
 fi
 echo "----> cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/"
 cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/
-echo "----> cp ~/git-projects/fedora-sh/rclone-all-list/[ilr]* ~/bin/"
-cp ~/git-projects/fedora-sh/rclone-all-list/[ilr]* ~/bin/
+echo "----> cp ~/git-projects/fedora-sh/rclone-all-list/[ilry]* ~/bin/"
+cp ~/git-projects/fedora-sh/rclone-all-list/[ilry]* ~/bin/
 echo "----> ls -al ~/bin"
 ls -al ~/bin
