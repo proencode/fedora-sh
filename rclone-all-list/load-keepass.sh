4f368a3f84251a4f73963d9ecfd3f764608270d6
yosj at u2204svr <yosjeon@gmail.com>
2024-03-12 16:09:33 +0900

    스크립트에 색상을 넣었습니다.

--- rclone-all-list/load-keepass.sh
+++ rclone-all-list/load-keepass.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+hhh=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2) #-- 230908
+
 keepass_name="keepass"
 cloud_name_dir="yswone:${keepass_name}"
 org_dir="${HOME}/wind_bada/downloads"
@@ -9,9 +11,9 @@ org_dir_kdbx="${org_dir}/${org_kdbx}"
 ym_kdbx="${keepass_name}$(date +%y%m%d-%H%M).kdbx"
 ym_dir_kdbx="${HOME}/${ym_kdbx}"
 
-echo "#----> rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/"
+echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/${xxx}"
 rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/
-echo "#----> ls -l ${org_dir}/${keepass_name}*.kdbx"
+echo "${bbb}#----> ${ccc}ls -l ${org_dir}/${keepass_name}*.kdbx${xxx}"
 ls -l ${org_dir}/${keepass_name}*.kdbx
-echo "#----> rclone lsl ${cloud_name_dir}/ | sort -k2 -k3"
+echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
 rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
--- rclone-all-list/save-keepass.sh
+++ rclone-all-list/save-keepass.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+hhh=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2) #-- 230908
+
 keepass_name="keepass"
 cloud_name_dir="yswone:${keepass_name}"
 org_dir="${HOME}/wind_bada/downloads"
@@ -24,15 +26,15 @@ __EOF__
         exit -1
 fi
 
-echo "#----> rclone lsl ${cloud_name_dir}/ | sort -k2 -k3"
+echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
 rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
-echo "#----> rclone copy ${org_dir_kdbx} ${cloud_name_dir}/"
+echo "${bbb}#----> ${ccc}rclone copy ${org_dir_kdbx} ${cloud_name_dir}/${xxx}"
 rclone copy ${org_dir_kdbx} ${cloud_name_dir}/
-echo "#----> cp ${org_dir_kdbx} ${ym_dir_kdbx}"
+echo "${bbb}#----> ${ccc}cp ${org_dir_kdbx} ${ym_dir_kdbx}${xxx}"
 cp ${org_dir_kdbx} ${ym_dir_kdbx}
-echo "#----> rclone copy ${ym_dir_kdbx} ${cloud_name_dir}/${ym}/"
+echo "${bbb}#----> ${ccc}rclone copy ${ym_dir_kdbx} ${cloud_name_dir}/${ym}/${xxx}"
 rclone copy ${ym_dir_kdbx} ${cloud_name_dir}/${ym}/
-echo "#----> rm -f ${ym_dir_kdbx}"
+echo "${bbb}#----> ${ccc}rm -f ${ym_dir_kdbx}${xxx}"
 rm -f ${ym_dir_kdbx}
-echo "#----> rclone lsl ${cloud_name_dir}/ | sort -k2 -k3"
+echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
 rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
