dff8fd1de59cab0548eff14235630c7350de2044
yosj at u2204svr <yosjeon@gmail.com>
2024-03-19 15:04:09 +0900

    ods 도 저장/로드 하도록 스크립트를 수정합니다.

--- rclone-all-list/save-keepass.sh
+++ rclone-all-list/save-keepass.sh
@@ -14,14 +14,32 @@ org_dir_kdbx="${org_dir}/${org_kdbx}"
 ym_kdbx="${keepass_name}$(date +%y%m%d-%H%M).kdbx"
 ym_dir_kdbx="${HOME}/${ym_kdbx}"
 
+org_ODS="43-${keepass_name}${ym}.ods"
+org_dir_ODS="${org_dir}/${org_ODS}"
+ym_ODS="43-${keepass_name}$(date +%y%m%d-%H%M).ods"
+ym_dir_ODS="${HOME}/${ym_ODS}"
+
 echo "#----> ls -l ${org_dir}/${keepass_name}*.kdbx"
 ls -l ${org_dir}/${keepass_name}*.kdbx
+echo "#----> ls -l 43-${org_dir}/${keepass_name}*.ods"
+ls -l ${org_dir}/43-${keepass_name}*.ods
 
 if [ ! -f ${org_dir_kdbx} ]; then
         cat <<__EOF__
 #-- ${org_dir_kdbx} 파일이 없습니다.
 #-- ${org_dir} 에서 ${keepass_name}-9912.kdbx 파일이 있으면,
 #-- ${org_kdbx} 로 이름을 바꾸고 다시 실행하세요~
+#-- ls -l ${org_dir}/
+$(ls -l ${org_dir}/)
+#--
+__EOF__
+        exit -1
+fi
+if [ ! -f ${org_dir_ODS} ]; then
+        cat <<__EOF__
+#-- ${org_dir_ODS} 파일이 없습니다.
+#-- ${org_dir} 에서 43-${keepass_name}-9912.ods 파일이 있으면,
+#-- ${org_ODS} 로 이름을 바꾸고 다시 실행하세요~
 #-- ls -l ${org_dir}/
 $(ls -l ${org_dir}/)
 #--
@@ -39,5 +57,18 @@ echo "${bbb}#----> ${ccc}rclone copy ${ym_dir_kdbx} ${cloud_name_dir}/${ym}/${xx
 rclone copy ${ym_dir_kdbx} ${cloud_name_dir}/${ym}/
 echo "${bbb}#----> ${ccc}rm -f ${ym_dir_kdbx}${xxx}"
 rm -f ${ym_dir_kdbx}
+echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
+rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
+
+echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
+rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
+echo "${bbb}#----> ${ccc}rclone copy ${org_dir_ODS} ${cloud_name_dir}/${xxx}"
+rclone copy ${org_dir_ODS} ${cloud_name_dir}/
+echo "${bbb}#----> ${ccc}cp ${org_dir_ODS} ${ym_dir_ODS}${xxx}"
+cp ${org_dir_ODS} ${ym_dir_ODS}
+echo "${bbb}#----> ${ccc}rclone copy ${ym_dir_ODS} ${cloud_name_dir}/${ym}/${xxx}"
+rclone copy ${ym_dir_ODS} ${cloud_name_dir}/${ym}/
+echo "${bbb}#----> ${ccc}rm -f ${ym_dir_ODS}${xxx}"
+rm -f ${ym_dir_ODS}
 echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
 rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
