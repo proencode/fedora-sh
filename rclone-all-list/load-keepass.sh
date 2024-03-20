4976f1cf05902fb4582184fa09247edcb2dbdde4
orangepi at opizero2w <yosjeon@gmail.com>
2024-03-20 13:46:44 +0900

    rclone copy 시 받는쪽에서 파일 이름까지 지정하는 것을 삭제합니다.

--- rclone-all-list/load-keepass.sh
+++ rclone-all-list/load-keepass.sh
@@ -15,8 +15,8 @@ org_dir_kdbx="${org_dir}/${org_kdbx}"
 ym_kdbx="${keepass_name}$(date +%y%m%d-%H%M).kdbx"
 ym_dir_kdbx="${HOME}/${ym_kdbx}"
 
-echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir_kdbx}${xxx}"
-rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir_kdbx}
+echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/${xxx}"
+rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/
 echo "${bbb}#----> ${ccc}ls -l ${org_dir}/${keepass_name}*.kdbx${xxx}"
 ls -l ${org_dir}/${keepass_name}*.kdbx
 #-- echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
@@ -27,8 +27,8 @@ org_dir_ODS="${org_dir}/${org_ODS}"
 ym_ODS="43-${keepass_name}$(date +%y%m%d-%H%M).ods"
 ym_dir_ODS="${HOME}/${ym_ODS}"
 
-echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir_ODS}${xxx}"
-rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir_ODS}
+echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir}/${xxx}"
+rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir}/
 echo "${bbb}#----> ${ccc}ls -l ${org_dir}/43-${keepass_name}*.ods${xxx}"
 ls -l ${org_dir}/43-${keepass_name}*.ods
 echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
