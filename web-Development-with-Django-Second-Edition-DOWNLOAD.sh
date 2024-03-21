29561795faa90dc1f59a648185bf1be179760613
yosj at u2204svr <yosjeon@gmail.com>
2024-03-21 11:41:01 +0900

    packtpub 의 django 소스 다운로드 스크립트를 추가합니다.

---
+++ web-Development-with-Django-Second-Edition-DOWNLOAD.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "----> git clone https://github.com/PacktPublishing/Web-Development-with-Django-Second-Edition.git"
+git clone https://github.com/PacktPublishing/Web-Development-with-Django-Second-Edition.git
