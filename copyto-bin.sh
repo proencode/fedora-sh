f6a64cadbb456a06bb05db161582d8d93c67fdb5
yosjeon at yscart <yosjeon@gmail.com>
2024-03-22 20:35:46 +0900

    copyto-bin 스크립트에서 복사를 rsync 로 바꿨습니다.

--- copyto-bin.sh
+++ copyto-bin.sh
@@ -4,11 +4,11 @@ if [ ! -d ~/bin ]; then
 	echo "----> mkdir ~/bin"
 	mkdir ~/bin
 fi
-echo "----> cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/"
-cp ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/
-echo "----> cp ~/git-projects/fedora-sh/rclone-all-list/[ilrsy]* ~/bin/"
-cp ~/git-projects/fedora-sh/rclone-all-list/[ilrsy]* ~/bin/
-echo "----> cp ~/git-projects/fedora-sh/copy* ~/bin/"
-cp ~/git-projects/fedora-sh/copy* ~/bin/
+echo "----> rsync -avzr ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/"
+rsync -avzr ~/git-projects/fedora-sh/bin-original/[7ad]* ~/bin/
+echo "----> rsync -avzr ~/git-projects/fedora-sh/rclone-all-list/[ilrsy]* ~/bin/"
+rsync -avzr ~/git-projects/fedora-sh/rclone-all-list/[ilrsy]* ~/bin/
+echo "----> rsync -avzr ~/git-projects/fedora-sh/copy* ~/bin/"
+rsync -avzr ~/git-projects/fedora-sh/copy* ~/bin/
 echo "----> ls -al ~/bin"
 ls -al ~/bin
