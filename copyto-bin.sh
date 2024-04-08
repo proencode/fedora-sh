f3c2c986c549d2677e8190d95d4eaac730b54deb
yosj at u2204svr <yosjeon@gmail.com>
2024-04-08 10:20:47 +0900

    copy 스크립트에 load.., rclone.., yesterday.. 스크립트 실행을 추가합니다.

--- copyto-bin.sh
+++ copyto-bin.sh
@@ -13,3 +13,10 @@ rsync -avzr ~/git-projects/fedora-sh/copy* ~/bin/
 echo "----> ls -al ~/bin"
 rsync -avzr ~/git-projects/fedora-sh/bin*/du-* ~/bin/
 ls -altr ~/bin
+
+echo "----> sh ~/bin/load-keepass.sh"
+sh ~/bin/load-keepass.sh
+echo "----> sh ~/bin/rclone-all-list.sh"
+sh ~/bin/rclone-all-list.sh
+echo "----> sh ~/bin/yesterday-rclone-lsl.sh"
+sh ~/bin/yesterday-rclone-lsl.sh 
