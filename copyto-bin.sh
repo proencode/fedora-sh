b47eb3f8568f9e4539b072d9a500613a33ad6bd8
yosj at u2204svr <yosjeon@gmail.com>
2024-04-08 13:29:56 +0900

    copy.. 스크립트에서 rc_lsl 디렉토리에 로그를 남기도록 수정합니다.

--- copyto-bin.sh
+++ copyto-bin.sh
@@ -16,6 +16,13 @@ ls -altr ~/bin
 
 echo "----> sh ~/bin/load-keepass.sh"
 sh ~/bin/load-keepass.sh
+rc_lsl=~/rclone-lsl
+if [ ! -d ${rc_lsl} ]; then
+	echo "----> mkdir ${rc_lsl}"
+	mkdir ${rc_lsl}
+fi
+echo "----> cd ${rc_lsl}"
+cd ${rc_lsl}
 echo "----> sh ~/bin/rclone-all-list.sh"
 sh ~/bin/rclone-all-list.sh
 echo "----> sh ~/bin/yesterday-rclone-lsl.sh"
