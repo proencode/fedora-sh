f1a12a00c0aacb3db38074dc0d44764a1f77975d
orangepi at opizero2w <yosjeon@gmail.com>
2024-04-20 16:01:47 +0900

    cc 스크립트에서 문자열 수정시 원본을 별도의 폴더에 넣도록 합니다.

--- 12-markdown-Archive/packtpub/16-2404a-Web_Dvlop_wt_Django/md_made_ing/cc-quote-character-check.sh
+++ 12-markdown-Archive/packtpub/16-2404a-Web_Dvlop_wt_Django/md_made_ing/cc-quote-character-check.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
 file=$1
-#-- “”
-char1="“"
-char2="”"
-found=0
+shcmd=""
 
-found_char=$(grep $char1 $file)
-if [ "x" != "x${found_char}" ]; then
-	found=1
-fi
-echo "1) found=${found};"
-found_char=$(grep $char2 $file)
-if [ "x" != "x${found_char}" ]; then
-	found=$((${found} + 1))
-fi
-echo "2) found=${found};"
+#-- " " '
 
-if [ ${found} > 0 ]; then
-	echo "cp ${file} ${file}-$(date +%y%m%d-%H%M) ; sed -i 's/${char1}/\"/g' ${file} ; sed -i 's/${char2}/\"/g' ${file} #-- 명령으로 --${char}-- 문자열을 바꾸세요."
+for char in "“" "”"
+do
+	found_char=$(grep ${char} ${file})
+	if [ "x" != "x${found_char}" ]; then
+		shcmd="${shcmd} ; sed -i 's/${char}/\\\"/g' ${file}"
+		#-- ---------------------(_________|^^^^|_)
+	fi
+done
+
+for char in "’"
+do
+	found_char=$(grep ${char} ${file})
+	if [ "x" != "x${found_char}" ]; then
+		shcmd="${shcmd} ; sed -i \"s/${char}/'/g\" ${file}"
+		#-- ---------------------(__________|^|__)
+	fi
+done
+
+if [ "x" != "x${shcmd}" ]; then
+	echo "rsync -avzr ${file} md_made_ing/old-files/${file}-$(date +%y%m%d-%H%M)${shcmd} #-- 문자열을 바꿉니다."
 fi
