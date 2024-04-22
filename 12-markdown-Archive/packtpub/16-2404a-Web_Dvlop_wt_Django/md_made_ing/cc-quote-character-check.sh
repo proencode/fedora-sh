e6af5708ea2af4386b430e6451f7e9a4a833c50a
orangepi at opizero2w <yosjeon@gmail.com>
2024-04-22 12:39:39 +0900

    문자열 바꾸는 cc 스크립트를 수정합니다.

--- 12-markdown-Archive/packtpub/16-2404a-Web_Dvlop_wt_Django/md_made_ing/cc-quote-character-check.sh
+++ 12-markdown-Archive/packtpub/16-2404a-Web_Dvlop_wt_Django/md_made_ing/cc-quote-character-check.sh
@@ -4,25 +4,44 @@ file=$1
 shcmd=""
 
 #-- " " '
+from_chars="#--from--"
+to_chars="#---to---"
 
 for char in "“" "”"
 do
 	found_char=$(grep ${char} ${file})
+	from_chars="${from_chars}${char}--"
 	if [ "x" != "x${found_char}" ]; then
 		shcmd="${shcmd} ; sed -i 's/${char}/\\\"/g' ${file}"
 		#-- ---------------------(_________|^^^^|_)
+		to_chars="${to_chars}\"--"
+	else
+		to_chars="${to_chars}??-"
 	fi
 done
 
 for char in "’"
 do
 	found_char=$(grep ${char} ${file})
+	from_chars="${from_chars}${char}--"
 	if [ "x" != "x${found_char}" ]; then
 		shcmd="${shcmd} ; sed -i \"s/${char}/'/g\" ${file}"
 		#-- ---------------------(__________|^|__)
+		to_chars="${to_chars}'--"
+	else
+		to_chars="${to_chars}!!-"
 	fi
 done
 
 if [ "x" != "x${shcmd}" ]; then
-	echo "rsync -avzr ${file} md_made_ing/old-files/${file}-$(date +%y%m%d-%H%M)${shcmd} #-- 문자열을 바꿉니다."
+	cat <<__EOF__
+#----> 문자열을 바꾸는 스크립트
+
+rsync -avzr ${file} md_made_ing/old-files/${file}-$(date +%y%m%d-%H%M)${shcmd} #-- 문자열을 바꿉니다.
+
+${from_chars}
+${to_chars}
+
+#<---- 문자열을 바꾸는 스크립트
+__EOF__
 fi
