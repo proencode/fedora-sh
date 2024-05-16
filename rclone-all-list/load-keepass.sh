dfe6b3b22d724096f2eed4b61f5053e97f8e0c30
yosjeon at yscart <yosjeon@gmail.com>
2024-05-16 23:49:08 +0900

    클라우드 백업 스크립트를 수정합니다.

--- rclone-all-list/load-keepass.sh
+++ rclone-all-list/load-keepass.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-hhh=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2) #-- 230908
+source ~/bin/cmdbox
 
 keepass_name="keepass"
 cloud_name_dir="yswone:${keepass_name}"
 org_dir="${HOME}/wind_bada"
 if [ ! -d ${org_dir} ]; then
-	mkdir -p ${org_dir}
+	cmdrun "mkdir -p ${org_dir}" "0. 로컬 디렉토리"
 fi
 ym="$(date +%y%m)"
 
@@ -15,21 +15,14 @@ org_dir_kdbx="${org_dir}/${org_kdbx}"
 ym_kdbx="${keepass_name}$(date +%y%m%d-%H%M).kdbx"
 ym_dir_kdbx="${HOME}/${ym_kdbx}"
 
-echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/${xxx}"
-rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/
-echo "${bbb}#----> ${ccc}ls -ltr ${org_dir}/${keepass_name}*.kdbx${xxx}"
-ls -ltr ${org_dir}/${keepass_name}*.kdbx
-#-- echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
-#-- rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
+cmdrun "rclone copy ${cloud_name_dir}/${org_kdbx} ${org_dir}/" "1. 클라우드에서 로컬로 받기"
+cmdrun "ls -ltr ${org_dir}/${keepass_name}*.kdbx" "2. 받은 파일"
 
 org_ODS="43-${keepass_name}${ym}.ods"
 org_dir_ODS="${org_dir}/${org_ODS}"
 ym_ODS="43-${keepass_name}$(date +%y%m%d-%H%M).ods"
 ym_dir_ODS="${HOME}/${ym_ODS}"
 
-echo "${bbb}#----> ${ccc}rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir}/${xxx}"
-rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir}/
-echo "${bbb}#----> ${ccc}ls -ltr ${org_dir}/43-${keepass_name}*.ods${xxx}"
-ls -ltr ${org_dir}/43-${keepass_name}*.ods
-echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3${xxx}"
-rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
+cmdrun "rclone copy ${cloud_name_dir}/${org_ODS} ${org_dir}/" "3. 클라우드에서 .ods 받기"
+cmdrun "ls -ltr ${org_dir}/43-${keepass_name}*.ods" "4. 받은 .ods 파일"
+cmdrun "rclone lsl ${cloud_name_dir}/ | sort -k2 -k3" "5. 클라우드 내용"
--- rclone-all-list/save-keepass.sh
+++ rclone-all-list/save-keepass.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-hhh=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2) #-- 230908
+source ~/bin/cmdbox
 
 home_dir="${HOME}/wind_bada"
 if [ ! -d ${home_dir} ]; then
-	mkdir -p ${home_dir}
+	cmdrun "mkdir -p ${home_dir}" "0. 새로운 디렉토리"
 fi
 
 cloud_BACKUP_name_dir="yswone:backup-keepass"
@@ -18,22 +18,19 @@ d_hm_kdbx="keepass${ymd_hm}.kdbx"
 ym_ods="43-keepass${ym}.ods"
 d_hm_ods="43-keepass${ymd_hm}.ods"
 
-echo "${bbb}#----> ${ccc}ls -ltr ${home_dir}/keepass*.kdbx${xxx}"
-ls -ltr ${home_dir}/keepass*.kdbx
-echo "${bbb}#----> ${ccc}ls -ltr 43-${home_dir}/keepass*.ods${xxx}"
-ls -ltr ${home_dir}/43-keepass*.ods
+cmdrun "ls -ltr ${home_dir}/keepass*.kdbx" "1. .kdbx 확인"
+cmdrun "ls -ltr ${home_dir}/43-keepass*.ods" "2. .ods 확인"
 
 if [ ! -f ${home_dir}/${ym_kdbx} ]; then
 	#-- ym_1_month_ago="$(date -d '1 month ago' +%y%m)" #-- 1개월 전: https://soft.plusblog.co.kr/97
 	ym_1_month_ago=$(date -d "1 month ago" +%y%m) #-- 1개월 전: https://soft.plusblog.co.kr/97
 	ym_1mAgo_kdbx="keepass${ym_1_month_ago}.kdbx"
 	if [ -f ${home_dir}/${ym_1mAgo_kdbx} ]; then
-		echo "${bbb}#----> ${ccc}mv ${home_dir}/${ym_1mAgo_kdbx} ${home_dir}/${ym_kdbx} ${bbb}#-- 파일이름 변경${xxx}"
-		mv ${home_dir}/${ym_1mAgo_kdbx} ${home_dir}/${ym_kdbx}
+		cmdrun "mv ${home_dir}/${ym_1mAgo_kdbx} ${home_dir}/${ym_kdbx}" "2.1 이번달로 .kdbx 이름변경"
 	else
 	        cat <<__EOF__
 ${bbb}
-#-- ${home_dir}/${ym_kdbx} 파일이 없습니다.
+#-- 2.2 ${home_dir}/${ym_kdbx} 파일이 없습니다.
 #-- ls -ltr ${home_dir}/
 $(ls -ltr ${home_dir}/)
 #--${xxx}
@@ -45,12 +42,11 @@ if [ ! -f ${home_dir}/${ym_ods} ]; then
 	ym_1_month_ago=$(date -d "1 month ago" +%y%m) #-- 1개월 전: https://soft.plusblog.co.kr/97
 	ym_1mAgo_ods="43-keepass${ym_1_month_ago}.ods"
 	if [ -f ${home_dir}/${ym_1mAgo_ods} ]; then
-		echo "${bbb}#----> ${ccc}mv ${home_dir}/${ym_1mAgo_ods} ${home_dir}/${ym_ods} ${bbb}#-- 파일이름 변경${xxx}"
-		mv ${home_dir}/${ym_1mAgo_ods} ${home_dir}/${ym_ods}
+		cmdrun "mv ${home_dir}/${ym_1mAgo_ods} ${home_dir}/${ym_ods}" "2.3 이번달로 .ods 변경"
 	else
 	        cat <<__EOF__
 ${bbb}
-#-- ${home_dir}/${ym_ods} 파일이 없습니다.
+#-- 2.4 ${home_dir}/${ym_ods} 파일이 없습니다.
 #-- ls -ltr ${home_dir}/${xxx}
 $(ls -ltr ${home_dir}/)
 ${bbb}#--${xxx}
@@ -59,49 +55,26 @@ __EOF__
 	fi
 fi
 
-echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3 ${bbb}#-- 옛 클라우드${xxx}"
-rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
+cmdrun "rclone lsl ${cloud_name_dir}/ | sort -k2 -k3" "3. 옛 클라우드 확인"
 
-echo "${bbb}#----> ${ccc}rclone move ${cloud_name_dir}/${ym}/ ${cloud_BACKUP_name_dir}/${ym}/ ${bbb}#-- 클라우드 옛자료 백업${xxx}"
-rclone move ${cloud_name_dir}/${ym}/ ${cloud_BACKUP_name_dir}/${ym}/
+cmdrun "rclone move ${cloud_name_dir}/${ym}/ ${cloud_BACKUP_name_dir}/${ym}/" "4. 클라우드 옛 자료 이동"
 
-echo "${bbb}#----> ${ccc}rclone lsl ${cloud_BACKUP_name_dir}/ | sort -k2 -k3 ${bbb}#-- 백업후 클라우드${xxx}"
-rclone lsl ${cloud_BACKUP_name_dir}/ | sort -k2 -k3
-
+cmdrun "rclone lsl ${cloud_BACKUP_name_dir}/ | sort -k2 -k3" "5. 이동후 클라우드"
 #--
+cmdrun "rclone copy ${home_dir}/${ym_kdbx} ${cloud_name_dir}/" "6. ym 을 클라우드로 복사"
 
-echo "${bbb}#----> ${ccc}rclone copy ${home_dir}/${ym_kdbx} ${cloud_name_dir}/ ${bbb}#-- 클라우드로 복사 4-1${xxx}"
-rclone copy ${home_dir}/${ym_kdbx} ${cloud_name_dir}/
+cmdrun "cp ${home_dir}/${ym_kdbx} ${home_dir}/${d_hm_kdbx}" "7. 로컬에 ymd_hm 으로 복사"
 
-echo "${bbb}#----> ${ccc}cp ${home_dir}/${ym_kdbx} ${home_dir}/keepass${ymd_hm}.kdbx ${bbb}#-- 로컬에 복사${xxx}"
-cp ${home_dir}/${ym_kdbx} ${home_dir}/keepass${ymd_hm}.kdbx
+cmdrun "rclone copy ${home_dir}/${d_hm_kdbx} ${cloud_name_dir}/${ym}/" "8. ymd_hm 을 클라우드로 복사"
 
+cmdrun "rm -f ${home_dir}/${d_hm_kdbx}" "9. ymd_hm 을 로컬에서 삭제"
 #--
+cmdrun "rclone copy ${home_dir}/${ym_ods} ${cloud_name_dir}/" "10. ym.ods 를 클라우드로 복사"
 
-echo "${bbb}#----> ${ccc}rclone copy ${home_dir}/keepass${ymd_hm}.kdbx ${cloud_name_dir}/${ym}/ ${bbb}#-- 클라우드로 복사 4-2${xxx}"
-rclone copy ${home_dir}/keepass${ymd_hm}.kdbx ${cloud_name_dir}/${ym}/
+cmdrun "cp ${home_dir}/${ym_ods} ${home_dir}/${d_hm_ods}" "11. 로컬에 ymd_hm.ods 로 복사"
 
-echo "${bbb}#----> ${ccc}rm -f ${home_dir}/keepass${ymd_hm}.kdbx ${bbb}#-- 로컬 삭제${xxx}"
-rm -f ${home_dir}/keepass${ymd_hm}.kdbx
+cmdrun "rclone copy ${home_dir}/${d_hm_ods} ${cloud_name_dir}/${ym}/" "12. ymd_hm.ods 를 클라우드로 복사"
 
+cmdrun "rm -f ${home_dir}/${d_hm_ods}" "13. ymd_hm.ods 를 로컬에서 삭제"
 #--
-
-echo "${bbb}#----> ${ccc}rclone copy ${home_dir}/${ym_ods} ${cloud_name_dir}/ ${bbb}#-- 클라우드로 복사 4-3${xxx}"
-rclone copy ${home_dir}/${ym_ods} ${cloud_name_dir}/
-
-echo "${bbb}#----> ${ccc}cp ${home_dir}/${ym_ods} ${home_dir}/${d_hm_ods} ${bbb}#-- 로컬에 복사${xxx}"
-cp ${home_dir}/${ym_ods} ${home_dir}/${d_hm_ods}
-
-#--
-
-echo "${bbb}#----> ${ccc}rclone copy ${home_dir}/${d_hm_ods} ${cloud_name_dir}/${ym}/ ${bbb}#-- 클라우드로 복사 4-4${xxx}"
-rclone copy ${home_dir}/${d_hm_ods} ${cloud_name_dir}/${ym}/
-
-echo "${bbb}#----> ${ccc}rm -f ${home_dir}/${d_hm_ods} ${bbb}#-- 로컬 삭제${xxx}"
-rm -f ${home_dir}/${d_hm_ods}
-
-#--
-
-echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3 ${bbb}#-- 새로운 클라우드${xxx}"
-rclone lsl ${cloud_name_dir}/ | sort -k2 -k3
-echo "${bbb}#----> ${ccc}rclone lsl ${cloud_name_dir}/ | sort -k2 -k3 ${bbb}#-- $(date +%y%m%d%a-%H%M) 클라우드${xxx}"
+cmdrun "rclone lsl ${cloud_name_dir}/ | sort -k2 -k3" "14. 새로운 클라우드 $(date +%y%m%d%a-%H%M)"
