b3d7f1fd19bd0723157223760ffa1a11c3af4a4f
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-05-29 15:36:41 +0900

    따옴표 빠진것을 추가합니다.

--- ping-0-to-logfile.sh
+++ ping-0-to-logfile.sh
@@ -12,7 +12,7 @@ if [ "x$a" != "x" ]; then
 	this_ip=${a}
 fi
 
-log_file="zz_ping_list_$(date +'%y%m%d%a-%H%M%S')
+log_file="zz_ping_list_$(date +'%y%m%d%a-%H%M%S')"
 echo "$(date +'%y%m%d%a-%H%M%S')" > ${log_file}
 
 for i in {11..199}
