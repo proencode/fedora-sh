de4465054d8b310f645572e6be9653b4ce6c0c27
yosjeon at yscart <yosjeon@gmail.com>
2024-07-03 12:03:43 +0900

    비번종료로 업그레이드 합니다.

--- git-pull-ALL-proj-dir.sh
+++ git-pull-ALL-proj-dir.sh
@@ -31,5 +31,5 @@ do
 done
 cat <<__EOF__
 ${cBlue}#${cYellow}  git config --global user.password " ATBBn9e5ZGpH6qk8yNbXfMUfeGe49D6AB2 " #-- bbop대아투코드네임${cReset}
-${cBlue}#${cYellow}  git config --global user.password " ghp_a4czNfMIeaYxc9ZmZY4IPYipjzjQb63fq "#-- 누가사스싸이다 expires on Wed, Jun 26 2024.${cReset}
+${cBlue}#${cYellow}  git config --global user.password " ghp_f4aSIiWBEQwqUrsZpNZH1hbUO3jbyc1St "#-- 마이쓰리띠스케줄러 expires on Wed, Jun 26 2024. https://github.com/settings/tokens  ${cReset}
 __EOF__
