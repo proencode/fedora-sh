ca3f74cd4652a44d1ebffa3a80b224b4f2266838
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-08-02 14:59:16 +0900

    스크립트를 수정합니다.

--- backup-to-opi-gdrive.sh
+++ backup-to-opi-gdrive.sh
@@ -29,6 +29,6 @@ rsync -avzr -e "ssh -p ${port}" ${file} ${user}@${addr}:${sdir}/
 echo "#-- ssh -p 65536 user@${addr} rclone copy ${sdir}/${file} cloud:keepass/ #-- 클라우드로 복사"
 ssh -p ${port} ${user}@${addr} rclone copy ${sdir}/${file} yosjgc:keepass/
 echo "#-- ssh -p 65536 user@${addr} rclone lsl cloud:keepass/${file} #-- 복사확인"
-echo "$(ls -l ${file} ; date +%y%m%d%a-%H%M) ${logmsg}"
+echo "$(ls -l ${file} ; date +%y%m%d%a-%H%M): ${logmsg}"
 ssh -p ${port} ${user}@${addr} rclone lsl yosjgc:keepass/${file}
 echo "#-- 위 내용을 keep.google.com 에 복사해둔다."
