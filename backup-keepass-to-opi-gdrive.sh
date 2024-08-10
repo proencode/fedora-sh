32b091b722e1a7d606053bc264722b48c4730aa9
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-08-10 17:47:38 +0900

    backup-keepass 에서 클라우드의 이름을 수정해야 합니다.

--- backup-keepass-to-opi-gdrive.sh
+++ backup-keepass-to-opi-gdrive.sh
@@ -1,34 +1,60 @@
 #!/bin/sh
 
-file="keepassproen.kdbx"
-user="orangepi"
-addr="proen.duckdns.org"
-sdir="archive/keepass"
-echo "#-- ${file} COPYTO ${user}@${addr}:${sdir} AND GDrive"
+file_name="keepassproen"
+file_ext="kdbx"
+file="${file_name}.${file_ext}"
+
 echo "#-- ${file} 파일이 있는곳에서 실행해야 한다."
-
-echo "#-- INPUT port no"
-echo "#--       ----"
-read -s port
-
-echo "#-- INPUT sdir: [ ${sdir} ]"
-echo "#--       ----"
-read a
-if [ "x$a" != "x" ]; then
-	sdir=$a
-fi
-sdir=$(echo "$sdir" | perl -pe 's/\/+$//')
-echo "#--> ${sdir}"
-
-echo "#-- INPUT 수정 메모: 'keepassxc 수정메모' 를 복사해서 붙인다."
-echo "#--       ----"
+echo "#-- INPUT 수정메모"
+echo "#--       --------"
 read logmsg
 
-echo "#-- rsync -avzr -e \"ssh -p 65536\" ${file} user@${addr}:${sdir}/ #-- 서버로 복사"
-rsync -avzr -e "ssh -p ${port}" ${file} ${user}@${addr}:${sdir}/
-echo "#-- ssh -p 65536 user@${addr} rclone copy ${sdir}/${file} cloud:keepass/ #-- 클라우드로 복사"
-ssh -p ${port} ${user}@${addr} rclone copy ${sdir}/${file} yosjgc:keepass/
-echo "#-- ssh -p 65536 user@${addr} rclone lsl cloud:keepass/${file} #-- 복사확인"
-echo "$(ls -l ${file} ; date +%y%m%d%a-%H%M): ${logmsg}"
-ssh -p ${port} ${user}@${addr} rclone lsl yosjgc:keepass/${file}
-echo "#-- 위 내용을 keep.google.com 에 복사해둔다."
+logmemo=$(date +%y%m%d%a-%H%M" ${logmsg}")
+echo "#----------"
+echo ""
+echo "${logmemo}"
+ls -l ${file}
+echo ""
+
+echo "#-- keepass 파일의 공유항목에 수정메모를 업데이트 하고,"
+echo "#-- 수정메모를 keep 에 붙인다."
+echo "#-- press Enter:"
+read a
+
+user="orangepi"
+addr="proen.duckdns.org"
+svr_ArKe="archive/keepass"
+echo "#--"
+echo "#-- ${file} COPYTO user@server:${svr_ArKe} AND GDrive"
+echo "#-- (1) INPUT port no"
+echo "#--           ----"
+read -s port
+
+echo "#-- (2) INPUT arc./kee.: [ ${svr_ArKe} ]"
+echo "#--           ---------"
+read a
+if [ "x$a" != "x" ]; then
+	svr_ArKe=$a
+fi
+svr_ArKe=$(echo "$svr_ArKe" | perl -pe 's/\/+$//')
+echo "#--> ${svr_ArKe}"
+
+file_date_ext=${file_name}-$(date +%y%m%d-%H%M).${file_ext}
+echo "#-- ssh -p ${port} ${user}@${addr} mv ./${svr_ArKe}/${file} ./${svr_ArKe}/backup/${file_date_ext} #-- (3) 서버의 최종 파일을 backup/ 으로 이동"
+ssh -p ${port} ${user}@${addr} mv ./${svr_ArKe}/${file} ./${svr_ArKe}/backup/${file_date_ext}
+
+echo "#-- rsync -avzr -e \"ssh -p 65536\" ${file} user@${addr}:${svr_ArKe}/ #-- (4) 서버로 복사"
+rsync -avzr -e "ssh -p ${port}" ${file} ${user}@${addr}:${svr_ArKe}/
+
+echo "#-- ssh -p ${port} ${user}@${addr} rclone lsl yosjgc:keepass/ --include "${file_name}*.${file_ext}" #-- (5) 서버 파일 확인"
+#  echo "#-- ssh -p ${port} ${user}@${addr} rclone lsl yosjgc:keepass/ --include "${file_name}*.${file_ext}" #-- (5) 서버 파일 확인"
+#  ssh -p ${port} ${user}@${addr} rclone lsl yosjgc:keepass/ --include "${file_name}*.${file_ext}"
+
+echo "#-- ssh -p ${port} ${user}@${addr} rclone moveto yosjgc:keepass/${file} yosjgc:keepass/backup/${file_date_ext} #-- (6) 클라우드 파일 이름바꾸기"
+ssh -p ${port} ${user}@${addr} rclone moveto yosjgc:keepass/${file} yosjgc:keepass/backup/${file_date_ext}
+
+echo "#-- ssh -p 65536 user@${addr} rclone copy ./${svr_ArKe}/${file} cloud:keepass/ #-- (7) 클라우드로 복사"
+ssh -p ${port} ${user}@${addr} rclone copy ./${svr_ArKe}/${file} yosjgc:keepass/
+
+echo "#-- ssh -p 65536 user@${addr} rclone lsl cloud:keepass/ --include "${file_name}*" #-- (8) 복사확인"
+ssh -p ${port} ${user}@${addr} rclone lsl yosjgc:keepass/ --include "${file_name}*"
---
+++ read-keepass-from-opi.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+file="keepassproen.kdbx"
+user="orangepi"
+addr="proen.duckdns.org"
+svrdir="ar*/ke*" #-- svrdir="archive/keepass"
+#--- localdir="${HOME}/Downloads/bada"
+#--- localdir=$(echo "$localdir" | perl -pe 's/\/+$//') #-- 마지막 '/' 를 삭제한다.
+localdir="" #-- 현재 위치에 파일을 받기로 한다.
+
+#--- if [ ! -d ${localdir} ]; then
+#--- 	echo "#-- mkdir ${localdir}"
+#--- 	mkdir ${localdir}
+#--- fi
+
+echo "#-- ${user}@${addr}:${svrdir} COPY TO ${file}"
+
+echo "#-- INPUT port no"
+echo "#--       ----"
+read -s port
+
+echo "#-- INPUT svrdir: [ ${svrdir} ]"
+echo "#--       ------"
+read a
+if [ "x$a" != "x" ]; then
+	svrdir=$a
+fi
+svrdir=$(echo "$svrdir" | perl -pe 's/\/+$//') #-- 마지막 '/' 를 삭제한다.
+echo "#--> ${svrdir}/"
+
+echo ""
+echo "#-- ssh -p 65536 user@${addr} ls -l ${svrdir}/${file}"
+ssh -p ${port} ${user}@${addr} ls -l ${svrdir}/${file}
+echo "#--                           -----|--------------- (1) 서버의 현재 파일"
+echo "                                   |"
+echo "                                   V"
+echo "#-- ls -l ${localdir}${file}"
+ls -l ${localdir}${file}
+echo "#--                     --------------------- (2) 로컬의 현재 파일"
+
+echo ""
+echo "#-- [ (1) 서버의 파일 시각이 (2) 로컬의 파일 시각보다 최신인 경우만 받아온다 ]"
+echo "#--       ----------------       ---------------- "
+echo ""
+echo "#-- rsync -avzr -e \"ssh -p 65536\" user@${addr}:${svrdir}/${file} ${localdir}${file}"
+echo "#-- 서버에서 받아온다."
+rsync -avzr -e "ssh -p ${port}" ${user}@${addr}:${svrdir}/${file} ${localdir}${file}
+
+echo ""
+echo "#-- ls -l ${localdir}${file}"
+ls -l ${localdir}${file}
+echo "#--                     --------------------- (3) 로컬의 최종 파일"
