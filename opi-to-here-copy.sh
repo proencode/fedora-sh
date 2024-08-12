263dbc7e1caf20fd8f5f9aeebb0ec5b0d5d052e5
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-08-12 14:57:38 +0900

    opi 스크립트를 수정합니다.

--- opi-to-here-copy.sh
+++ opi-to-here-copy.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
-keeps_name="keepassproen" #-- keepass 파일의 이름만
-keeps_ext="kdbx" #-- 확장자
-keepsNameExt="${keeps_name}.${keeps_ext}" #-- keepass 파일명 전체
-
 userID="orangepi" #-- 호스트의 사용자
 svrURL="proen.duckdns.org" #-- 호스트 URL
 svrDIR="picture" #-- 호스트 파일이 있는 디렉토리
 
 cat <<__EOF__
-#-- !!! 호스트 파일을 받을 로컬 디렉토리 에서 실행해야 한다. !!!
+#-- !!! 호스트 파일을 내려받을 로컬 디렉토리 에서 실행해야 한다. !!!
 
 #-- userID@svrURL:${svrDIR} COPY TO current Directory
-#-- (1) INPUT: port no (서버 포트번호 입력시 숫자 표시 안됨)
+#-- (1) INPUT: port no (서버쪽 포트번호 입력시 숫자 표시 안됨)
 #--            ----
 __EOF__
 read -s svrPORT #-- 호스트 접속시 포트번호, '-s' 타이핑하는 글자를 안보여준다.
 
 cat <<__EOF__
-#-- (2) INPUT: 받을 파일의 서버 디렉토리 지정 [ ${svrDIR} ]
-#--                        -------------
+#-- (2) INPUT: 내려받을 파일의 서버쪽 디렉토리 지정 [ ${svrDIR} ]
+#--                            ---------------
 __EOF__
 read a
 
