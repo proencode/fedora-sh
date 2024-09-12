18b5eaf9b71b6d17d55e820dc363014991dbcc59
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-09-12 12:40:36 +0900

    lsl 파일에서 특정 문자열을 grep + uniq 로 걸러내는 스크립트를 추가합니다.

---
+++ grep-uniq-from-lsl.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
+
+cmdrun () {
+	echo "${yyy}# ----> ${ccc}$1 ${ggg}#-- ${bbb}$2${xxx}"; echo "$1" | sh
+	echo "${yyy}# <~~~~ ${bbb}$1 #-- $2${xxx}"
+}
+
+
+grepString="tradepub"
+
+echo "${yyy}#-- ${bbb}grep String [ ${ccc}${grepString}${bbb} ] press Enter:${xxx}"
+read a
+if [ "x$a" != "x" ]; then
+	grepString="$a"
+fi
+
+cmdrun "grep \"${grepString}\" *l | awk -F\":\" '{print \$4}' | awk '{print \$2}' | uniq"
