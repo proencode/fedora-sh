7298d1c28a61dc6a4d90a98b07f82b4bc4fcc0f5
yosj at u24041desk <yosjeon@gmail.com>
2024-11-13 16:54:35 +0900

    연습용 python 스크립트 입니다.

---
+++ python-study/lib-read-00.py
@@ -0,0 +1,7 @@
+with open("8-line-찬란한_멸종-리스트.txt", "r") as f:
+    for line in f:
+        print("----")
+        print(line)
+        print("....")
+        print(line.strip())
+        print("----")
