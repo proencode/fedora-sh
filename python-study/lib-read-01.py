c11ef99099a3d7735c1ecda28a004a94838f7770
yosj at u24041desk <yosjeon@gmail.com>
2024-11-13 17:17:42 +0900

    도서관 목록을 제목, 년도, 도서관명 으로 정리하는 스크립트를 추가합니다.

--- python-study/lib-read-01.py
+++ python-study/lib-read-01.py
@@ -4,14 +4,20 @@ file_name=sys.argv[1] # 8-line-찬란한_멸종-리스트.txt
 book_no=0 # 책의 일련번호
 jul_tot=8 # 책 하나의 설명줄 갯수
 line_seq=1 # 설명줄 순서
-# line456_view=0 # 표시하지 말것
+line3456_view=0 # 표시하지 말것
 
 with open(file_name, "r") as f:
     for a_line in f:
         if line_seq == 1: # 설명줄의 처음이면,
             book_no = book_no + 1
-            line456_view=0 # 표시하지 말것
             # print("---- [ %d ]" % book_no)
+            if line3456_view == 1: # 표시할것
+                print("---- [ %s %s %s %s ]" % (line3_str, line4_str, line5_str, line6_str))
+                line3_str=""
+                line4_str=""
+                line5_str=""
+                line6_str=""
+                line3456_view=0 # 표시하지 말것
         # else: # 처음이 아니면,
             # print("---- (%d-%d)" % (book_no, line_seq))
 
@@ -30,23 +36,29 @@ with open(file_name, "r") as f:
             s1=a_line.split('.')
             s2=s1[1].split(':')
             # print("%s.....%s" % (s2[0].strip(), a_line.strip())) # 설명중 1줄 표시
-            print("%s.....%s" % (s2[0].strip(), a_line.strip())) # 설명중 1줄 표시
-            line456_view=1 # 표시할것
+            line3_str=s2[0].strip() # 설명중 1줄 표시
+            line3456_view=1 # 표시할것
 
-        if line456_view == 1: # 표시할것
+        if line3456_view == 1: # 표시할것
             if line_seq == 4: # ---- 4 ---- 발행년도: 2004
                 # ------------------------- ^^^^^^^^|^^^^^
                 s1=a_line.split('발행년도:')
-                print("%s" % s1[1].strip()) # 설명중 1줄 표시
+                # print("%s" % s1[1].strip()) # 설명중 1줄 표시
+                line4_str=s1[1].strip() # 설명중 1줄 표시
+                line3456_view=1 # 표시할것
             if line_seq == 5: # ---- 5 ---- 청구기호: 004.575-성66홈
                 # ------------------------- ^^^^^^^^|^^^^^^^^^^
                 s1=a_line.split(':')
-                print("%s" % s1[1].strip()) # 설명중 1줄 표시
+                # print("%s" % s1[1].strip()) # 설명중 1줄 표시
+                line5_str=s1[1].strip() # 설명중 1줄 표시
+                line3456_view=1 # 표시할것
             if line_seq == 6: # ---- 6 ---- 도서관: 남양주시화도도서관 자료실: 화도문헌보존2
                 # ------------------------- ^^^^^^|^^^^^^^^^^^^^^^^^^^^^^^^^^|^^^^^^^^^^
                 s1=a_line.split(':')
                 s2=s1[1].strip().split('자료실:')
-                print("%s" % s2[0]) # 설명중 1줄 표시
+                # print("%s" % s2[0]) # 설명중 1줄 표시
+                line6_str=s2[0].strip() # 설명중 1줄 표시
+                line3456_view=1 # 표시할것
 
         # print(a_line.strip()) # 설명중 1줄 표시
 
