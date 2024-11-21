ca77f9d677ec8d93212f6744eaa3ff2b1b7c6216
yosj at u24041desk <yosjeon@gmail.com>
2024-11-21 16:49:23 +0900

    33-docker 스크립트를 수정합니다.

--- 11-wiki.js-Backup/33-docker-reinstall-wikijs.sh
+++ 11-wiki.js-Backup/33-docker-reinstall-wikijs.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 
-echo "#-- sudo docker ps -a #-- 현재 도커 작업"
-sudo docker ps -a
-echo "#-- sudo docker stop wikijs wikijsdb #-- 도커 정지"
-sudo docker stop wikijs wikijsdb
-echo "#-- sudo docker rm wikijs wikijsdb #-- 도커 삭제"
-sudo docker rm wikijs wikijsdb
-echo "#-- sudo docker ps -a #-- 현재 도커 작업"
-sudo docker ps -a
-echo "#---- for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done"
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-echo "#------------"
+lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
+
+cmdrun () {
+	echo "${yyy}# ----> ${ccc}$1 ${ggg}#-- ${bbb}$2${xxx}"; echo "$1" | sh
+	echo "${yyy}# <~~~~ ${bbb}$1 #-- $2${xxx}"
+}
+
+cmdrun "sudo docker ps -a" "(1-1) 현재 도커 작업"
+cmdrun "sudo docker stop wikijs wikijsdb" "(1-2) 도커 정지"
+cmdrun "sudo docker rm wikijs wikijsdb" "(1-3) 도커 삭제"
+cmdrun "sudo docker ps -a" "(1-4) 현재 도커 작업"
+cmdrun "for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done" "(1-5) 이전 버전 제거"
 echo "#------------ 1. 이전 버전 제거"
+echo "#------------"
 
-echo "#---- Add Docker's official GPG key:"
-echo "#---- sudo apt-get update"
-sudo apt-get update
-echo "#---- sudo apt-get install ca-certificates curl"
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+cmdrun "sudo apt-get update" "(2-1) Add Docker's official GPG key:"
+cmdrun "sudo apt-get install ca-certificates curl" "(2-2)"
+cmdrun "sudo apt-get install ca-certificates curl" "(2-3)"
+cmdrun "sudo install -m 0755 -d /etc/apt/keyrings" "(2-4)"
+cmdrun "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc" "(2-5)"
+cmdrun "sudo chmod a+r /etc/apt/keyrings/docker.asc" "(2-6)"
 
-echo "#---- Add the repository to Apt sources:"
+echo "${yyy}# ----> ${ccc}echo ... | sudo tee /etc/apt/sources.list.d/docker.list ${ggg}#-- ${bbb}(2-7) Add the repository to Apt sources: ${xxx}"
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo "#---- sudo apt-get update"
-sudo apt-get update
-echo "#------------"
+cmdrun "sudo apt-get update" "(2-8)"
 echo "#------------ 2. Docker 저장소를 설정"
-
-echo "#---- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "#------------"
+
+cmdrun "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin" "(3-1)"
 echo "#------------ 3. Docker 패키지를 설치"
+echo "#------------"
 
 y4=$(date +%Y) #-- 2023
 
-echo "#---- ls -R /home/docker"
-ls -R /home/docker
-echo "#----> /home/docker 디렉토리를 삭제합니다. press Enter:"
+cmdrun "ls -R /home/docker" "(4-1) 삭제하려는 디렉토리 확인"
+echo "${rrr}# ----> ${ccc}(4-2) /home/docker 디렉토리를 삭제합니다. ${mmm}press Enter:${xxx}"
 read a
-sudo rmdir -p /home/docker
-sudo mkdir -p /home/docker/backup/${y4} /home/docker/compose /home/docker/pgreswiki
-sudo chown -R ${USER}:${USER} /home/docker/backup
-sudo chmod -R 755 /home/docker/backup
-sudo chown ${USER}:${USER} /home/docker/compose /home/docker/backup/${y4}
+cmdrun "sudo rm -rf /home/docker" "(4-3)"
+cmdrun "sudo mkdir -p /home/docker/backup/${y4} /home/docker/compose /home/docker/pgreswiki" "(4-4)"
+cmdrun "sudo chown -R ${USER}:${USER} /home/docker/backup" "(4-5)"
+cmdrun "sudo chmod -R 755 /home/docker/backup" "(4-6)"
+cmdrun "sudo chown ${USER}:${USER} /home/docker/compose /home/docker/backup/${y4}" "(4-7)"
+echo "#------------ 4. 디렉토리를 지우고 새로 만듬."
+echo "#------------"
+
 cat > /home/docker/compose/docker-compose.yml <<__EOF__
 version: "3"
 services: #-- orangepi59zero2w 240214-1424
@@ -85,31 +85,33 @@ services: #-- orangepi59zero2w 240214-1424
     container_name:
       wikijs #wikicntnr #-- old_name= wikijs #-- wiki: 컨테이너 이름
 __EOF__
-ls -alR /home/docker/
-echo "#------------"
+cmdrun "sudo ls -alR /home/docker/" "(5-1)"
+echo "${rrr}# ----> ${ccc}(5-2) 디렉토리를 다시 만들었습니다. ${mmm}press Enter:${xxx}"
+read a
 echo "#------------ 5. Wiki.js 디렉토리 만들고, yml 저장"
+echo "#------------"
 
+echo "${yyy}# ----> ${ccc}(6-1) cd /home/docker/compose${xxx}"
 cd /home/docker/compose
 #-- time sudo docker-compose pull wikijs
-sudo docker compose up -d
+cmdrun "sudo docker compose up -d" "(6-2) wikijs 실행"
+echo "#------------ 6. wiki.js 도커 실행"
 echo "#------------"
-echo "#------------ 6. wiki.js 실행"
 
 if [ "x$1" = "x" ] || [ ! -f ${up_file} ]; then
-	echo "#------------"
-	echo "#------------ ls ~/wind*/wiki*/wiki*z*"
-	ls ~/wind*/wiki*/wiki*z*
-	echo "#------------ 7. 업로드할 파일 선택:"
+	cmdrun "ls ~/wind*/wiki*/wiki*z*" "(7-1) 업로드할 파일 목록보기"
+	echo "${rrr}# ----> ${ccc}(7-2) 업로드할 파일이름을 입력하세요.${xxx}"
 	read up_file
 	if [ "x${up_file}" = "x" ] || [ ! -f ${up_file} ]; then
-		echo "#-- ${up_file} 이 없습니다."
+		echo "#-- ${up_file}(7-3) 파일이 없습니다."
 		exit -1
 	fi
 else
 	up_file="$1"
 fi
-
+echo "#------------ 7. 업로드할 파일 선택"
 echo "#------------"
-echo "#------------ bash ~/bin/31-last-12-sql.7z--RESTORE-to--wikijs-DB.sh ${up_file}"
-bash ~/bin/31-last-12-sql.7z--RESTORE-to--wikijs-DB.sh ${up_file}
+
+cmdrun "bash ~/bin/31-last-12-sql.7z--RESTORE-to--wikijs-DB.sh ${up_file}" "(8-1) wiki DB 업로드"
 echo "#------------ 8. 업로드 작업"
+echo "#------------"
