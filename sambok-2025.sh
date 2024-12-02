25af1ada6edc68b726dac5169df9f79411a13fc0
orangepi at orangepi59zero2w <yosjeon@gmail.com>
2024-12-02 10:37:54 +0900

    sambok 스크립트를 일부 고칩니다.

--- sambok-2025.sh
+++ sambok-2025.sh
@@ -1,9 +1,18 @@
-y=2025; wol=101
+#!/bin/sh
+
+y=2025
+wol=101
 for jolgi in "소한5 대한20" "입춘3 우수18" "경칩5 춘분20" "청명4 곡우20" "입하5 소만21" "망종5 하지21" "소서7 대서22 초복20 중복30" "말복9 입추7 처서23" "백로7 추분23" "한로8 상강23" "입동7 소설22" "대설7 동지22" "소한5 대한20"
 do
-        echo "${y}-${wol:1} 오늘"
-        echo "${y}-${wol:1} ${jolgi}"
-        echo "ilji/${y}/${y:2}${wol:1}"
+	if [ $wol -eq "101" ]; then
+	        echo "${y}-${wol:1} 오늘 -->>>-- ${y}-${wol:1} 일지"
+		echo "${jolgi}"
+		echo "home -->>>-- ilji/${y}/${y:2}${wol:1}"
+	else
+	        echo "${y}-${wol:1} 오늘"
+		echo "${jolgi}"
+		echo "home"
+	fi
         echo " "
         wol=$(($wol+1))
         if [ $wol -gt "112" ]; then
