51623b52bccf1b24cc11f9e470860438e9e59b17
yosj at gigamaster <yosjeon@gmail.com>
2025-01-18 11:58:23 +0900

    bin 파일이 git_fedora 파일과 일치하는지 확인하는 스크립트를 추가합니다.

---
+++ bin/01-diff-bin-git_fedora.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
+cmdrun () {
+        echo "${yyy}#-- ${ccc}$1 ${ggg}#-- ${bbb}$2${xxx}"; echo "$1" | bash
+        echo "${yyy}#// ${bbb}$1 #-- $2${xxx}"
+}
+cmdend () {
+        echo "${bbb}#--///-- ${mmm}$1${xxx}"
+}
+
+			
+temp_file="temp-$(date +%y%m%d-%H%M%S)"
+from_dir="git-projects/fedora-sh/bin"
+to_dir="bin"
+ls -p ~/${to_dir}/ | grep -v '/$' | while read each_file_name #-- 파일 이름만 한개씩 가져오기
+do
+	name_str=$(echo ${each_file_name} | sed 's/ /\\ /g')
+	if [ ! -f ~/${from_dir}/${name_str} ]; then
+        echo "${ggg}#-- ${ggg}~/${from_dir}/${name_str} ${ggm}#-- ${mmm}파일이 없음${xxx}"
+	else
+		diff ~/${from_dir}/${name_str} ~/${to_dir}/${name_str} > ${temp_file}
+		if [ "x$(du ${temp_file} | awk '{print $1}')" == "x0" ]; then
+   	     echo "${ggg}#-- ${ggg}${name_str} ${ggg}#-- ${ggg}일치함${xxx}"
+		else
+			cmdrun "diff ~/${from_dir}/${name_str} ~/${to_dir}/${name_str}; ls -l ~/${from_dir}/${name_str} ~/${to_dir}/${name_str}" "${name_str} 비교"
+		fi
+	fi
+done
+cmdrun "rm -f ${temp_file}" "임시파일 삭제"
+cmdend "${0}"
+#--
+#--
+#--
