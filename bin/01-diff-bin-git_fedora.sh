1baed77ac525f35a1d397964552fe77638671aef
yosj at u24041svr-VB <yosjeon@gmail.com>
2025-02-02 15:36:40 +0900

    01- 스크립트에서 git status 확인하도록 대기를 추가합니다.

--- bin/01-diff-bin-git_fedora.sh
+++ bin/01-diff-bin-git_fedora.sh
@@ -3,7 +3,7 @@
 lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
 cmdrun () {
         echo "${yyy}#-- ${ccc}$1 ${ggg}#-- ${bbb}$2${xxx}"; echo "$1" | bash
-        echo "${yyy}#// ${bbb}$1 #-- $2${xxx}"
+        echo "${rrr}#// ${bbb}$1 #-- $2${xxx}"
 }
 cmdend () {
         echo "${bbb}#--///-- ${mmm}$1${xxx}"
@@ -13,6 +13,12 @@ cmdend () {
 temp_file="temp-$(date +%y%m%d-%H%M%S)"
 from_dir="git-projects/fedora-sh/bin"
 to_dir="bin"
+
+cmdrun "cd ~/git-projects/fedora-sh/; git status ; git pull; git ll -1; cd -" "최근의 bin 가져오기"
+echo "${yyy}#-- ${ccc}Press Enter${bbb}: (변경 사항이 있으면, 여기서 중단하고 마무리 해 줘야 합니다.)${xxx}"
+read a
+echo "${rrr}#// ${bbb}Press Enter: (변경 사항이 있으면, 여기서 중단하고 마무리 해 줘야 합니다.)${xxx}"
+
 ls -p ~/${to_dir}/ | grep -v '/$' | while read each_file_name #-- 파일 이름만 한개씩 가져오기
 do
 	name_str=$(echo ${each_file_name} | sed 's/ /\\ /g')
