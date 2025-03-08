bfbfa53c6c4338d82c4f8e222588d95133306532
orangepi at opi59zero2w <yosjeon@gmail.com>
2025-03-08 10:12:00 +0900

    01 스크립트를 고쳤습니다. 4

--- bin/01-diff-bin-git_fedora.sh
+++ bin/01-diff-bin-git_fedora.sh
@@ -12,6 +12,7 @@ cmdend () {
 			
 temp_file="temp-$(date +%y%m%d-%H%M%S)"
 from_dir="git-projects/fedora-sh/bin"
+from_dir="git-*/fed*/bin"
 to_dir="bin"
 
 cmdrun "cd ~/git-projects/fedora-sh/; git status ; git pull; git ll -1; cd -" "최근의 bin 가져오기"
@@ -25,14 +26,20 @@ do
 	name_str=$(echo ${each_file_name} | sed 's/ /\\ /g')
 	if [ ! -f ~/${from_dir}/${name_str} ]; then
 		to_seq=$((to_seq+1)) #-- from_seq=$((from_seq+1))
-		echo "${ggg}#-- ${rrr}${name_str}${mmm} #-- ${rrr}~/bin 에는 있으나 ${bbb}~/git- 에는 없음 ${rrr}${from_seq}${mmm}:${bbb}${to_seq}${xxx}"
+		echo "${ggg}#-- ${rrr}${name_str}${ggg} #-- ${rrr}~/bin 에는 있으나 ${from_seq}${ggg}:${bbb}${to_seq} ${bbb}~/git- 에는 없음${xxx}"
 	else
 		to_seq=$((to_seq + 1)); from_seq=$((from_seq + 1))
+		if [ "${to_seq}" = "${from_seq}" ]; then
+			seq_view="${bbb}${to_seq}"
+		else
+			seq_view="${rrr}${from_seq}${ggg}:${bbb}${to_seq}"
+		fi
 		diff ~/${from_dir}/${name_str} ~/${to_dir}/${name_str} > ${temp_file}
 		if [ "x$(du ${temp_file} | awk '{print $1}')" == "x0" ]; then
-			echo "${ggg}#-- ${ggg}${name_str} ${ggg}#-- ${ggg}일치함 ${bbb}${from_seq}${rrr}:${bbb}${to_seq}${xxx}"
+			echo "${ggg}#-- ${ggg}${name_str} ${ggg}#-- ${ggg}일치함 ${seq_view}${xxx}"
 		else
-			cmdrun "diff ~/${from_dir}/${name_str} ~/${to_dir}/${name_str}; ls -l ~/${from_dir}/${name_str} ~/${to_dir}/${name_str}" "${name_str} 비교 ${from_seq}:${to_seq}${xxx}"
+			cmdrun "diff ~/${from_dir}/${name_str} ~/${to_dir}/${name_str}" "${bbb}${seq_view}${xxx}"
+			cmdrun "ls -l ~/${from_dir}/${name_str} ~/${to_dir}/${name_str}"
 			echo "${rrr}rsync ${mmm}-avzr ${ccc}~/${from_dir}/${name_str} ${yyy}~/${to_dir}/${name_str};${xxx}    #--"
 		fi
 	fi
