b264e6110c7d316cc02b8d17c5a91e6fe545c086
orangepi at opi59zero2w <yosjeon@gmail.com>
2025-03-08 14:06:41 +0900

    qna 제목을 기록하는 .md 파일을 만드는 03- tmzmflqxmfmf cnrkgkqslek.

---
+++ bin/03-qna-chrome_extension-made.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
+cmdrun () {
+        echo "${yyy}#-- ${ccc}$1 ${mmm}#-- ${bbb}$2${xxx}"; echo "$1" | sh
+        echo "${bbb}#// $1 #-- $2${xxx}"
+}
+cmdend () {
+        echo "${bbb}#--///-- ${mmm}$1${xxx}"
+}
+cmdreada_s () { #-- cmdreada_s "(1) INPUT: port no" "(입력시 표시 안됨)"
+        echo "${yyy}#-- ${ccc}$1 ${ggg}#-- ${bbb}$2${xxx}"
+        read -s reada_s
+}
+cmdreada () { #-- cmdreada "(2) INPUT: domain name" "호스트 주소 입력"
+        echo "${yyy}#-- ${ccc}$1 ${mmm}#-- ${bbb}$2${xxx}"
+        read reada
+}
+
+begin_no=101
+cmdreada "INPUT: QA노트 시작 번호 (3자리 수)" "그냥 Enter 면, ${rrr}[ ${xxx}${begin_no} ${rrr}]"
+if [ "x${reada}" = "x" ]; then
+    reada=${begin_no}
+fi
+begin_no=${reada}
+
+end_no=120
+cmdreada "INPUT: QA노트 끝 번호 (3자리 수)" "그냥 Enter 면, ${rrr}[ ${xxx}${end_no} ${rrr}]"
+if [ "x${reada}" = "x" ]; then
+    reada=${end_no}
+fi
+end_no=${reada}
+
+date_mark=$(date "+%y%m%d(%a) %H%M")
+a_mark="$(date +%m%d%H)"
+id_mark="gemini${a_mark}"
+#--------^^^^^^
+file_name="qna-chrome-$(date +%y%m%d-%H%M).md"
+
+cat >> ${file_name} <<__EOF__
+
+- ${date_mark} 질문 모음 (qq)
+
+## 🔥 ${id_mark}-${begin_no:1}.
+__EOF__
+start_no=$((begin_no + 1))
+for (( i=start_no; i<=end_no; i++ ))
+do
+    cat >> ${file_name} <<__EOF__
+### 🔥 ${id_mark}-${i:1}.
+__EOF__
+done
+cmdrun "cat ${file_name}"
+cat <<__EOF__
+
+${yyy}vi ${ccc}${file_name}    ${bbb}#--///--
+
+__EOF__
+
+cat >> ${file_name} <<__EOF__
+
+- ${date_mark} 질문과 답변 (qna)
+
+## 🔥 ${id_mark}-${begin_no:1}.
+### 🔋 ${a_mark}-${begin_no:1}.
+__EOF__
+start_no=$((begin_no + 1))
+for (( i=start_no; i<=end_no; i++ ))
+do
+    cat >> ${file_name} <<__EOF__
+### 🔥 ${id_mark}-${i:1}.
+### 🔋 ${a_mark}-${i:1}.
+__EOF__
+done
+cmdrun "cat ${file_name}"
+cat <<__EOF__
+
+${yyy}vi ${ccc}${file_name}    ${bbb}#--///--
+
+__EOF__
+#--
+#--
+#--
