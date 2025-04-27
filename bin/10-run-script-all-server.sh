9c48bd94b7d1176fd4169c962ab4b4dcb1860323
proenpi at pi4b <yosjeon@gmail.com>
2025-04-27 13:07:31 +0900

    지정한 서버 전체에 동일 명령을 실행하는 10- 스크립트를 추가하고 ~/bin 에도 복사하도록 08- tmzmflqxmeh tnwjdgkqslek.

---
+++ bin/10-run-script-all-server.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+lll=$(tput bold)$(tput setaf 0); rrr=$(tput bold)$(tput setaf 1); ggg=$(tput bold)$(tput setaf 2); yyy=$(tput bold)$(tput setaf 3); bbb=$(tput bold)$(tput setaf 4); mmm=$(tput bold)$(tput setaf 5); ccc=$(tput bold)$(tput setaf 6); www=$(tput bold)$(tput setaf 7); xxx=$(tput bold)$(tput sgr0); uuu=$(tput cuu 2)
+cmdrun () {
+        echo "${yyy}#-- ${ccc}$1 ${mmm}#-- ${bbb}$2${xxx}"; echo "$1" | bash
+        echo "${bbb}#// $1 #-- $2${xxx}"
+}
+cmdend () {
+        echo "${bbb}#--///-- ${mmm}$1${xxx}"
+}
+
+
+for i in yosj@gg yosj@ryw proenpi@rp orangepi@yow orangepi@myw orangepi@nlw orangepi@mnw
+do
+	echo "${bbb}#-- ${ccc}ssh ${i} sudo -S apt update && sudo -S apt upgrade -y${xxx}"
+	ssh ${i} sudo -S apt update && sudo -S apt upgrade -y
+done
+#--
+#-- for i in yosj@gg yosj@ryw proenpi@rp orangepi@yow orangepi@myw orangepi@nlw orangepi@mnw; do if [ "$i" = "${USER}@$(hostname)" ]; then echo "#-- Skip ${i}"; else ssh $i; fi; done
+#--
