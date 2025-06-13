4f492c21d5e955dabc3517ad2990df0d89f93af4
proenpi at pi4b <yosjeon@gmail.com>
2025-06-13 12:05:49 +0900

    01-은 서버의 last- 를 여기로 복사, 02-는  99-를 백업하는 스크립트를 추가합니다.

---
+++ qna-chrome-extension-md/2506/last-250613-1105/02-saveto-number.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "#-- sh 02-saveto-number.sh ()"
+echo "#-- "
+if [[ "x${1}" < "x00" || "x${1}" > "x99" ]]; then
+	echo "#-- sh 02-saveto-number.sh 00"
+	echo "#-- "
+	echo "#-- 입력값은 "00" ~ "99" 사이의 값이라야 합니다."
+else
+	a="000${1}" #-- ${a: -2} == 뒤에서 2개를 꺼낸다.
+        dd="cusr13.1156-${a: -2}"
+        if [ -d ${dd} ]; then
+                echo "#-- ${dd} 폴더가 있어서 백업할 수 없습니다."
+        else
+                echo "#-- rsync -avzr cusr13.1156-99 cusr13.1156-${a: -2}"
+                rsync -avzr cusr13.1156-99 cusr13.1156-${a: -2}
+                echo "#-- ls -l --color"
+                ls -l --color
+        fi
+fi
