3eb08285542caff49e387195f2c2f55fbe5cde56
yosj at u24042svr-vb <yosjeon@gmail.com>
2025-08-08 13:22:49 +0900

    docker-compose 첫버전과 restore- 스크립트를 수정합니다.

--- bin/restore-wikijs-from-backup-250808_금-1230.sh
+++ bin/restore-wikijs-from-backup-250808_금-1230.sh
@@ -13,34 +13,52 @@ enter_value() {
 }
 
 enter_passwd() {
-	retv="$2"; echo "${mmm}#-- ${ggg}$1 [ $2 ]${xxx}" >&2 # >&2를 사용해 stderr 로 출력
+	retv="$2"; echo "${mmm}#-- ${ggg}$1 [ ...... ]${xxx}" >&2 # >&2를 사용해 stderr 로 출력
 	read -s a; if [ "x$a" != "x" ]; then retv=$a; fi #-- 비번은 화면에 표시 안함
 	echo "$retv"
 }
 
-qq=10
-DOCKER_ROOT=$(enter_value "${qq}-1. 도커의 루트 디렉토리:" "/home/docker")
-echo "${rrr}[ ${xxx}${DOCKER_ROOT} ${rrr}]${xxx}"
-BACKUP_DIR=$(enter_value "${qq}-2. 백업폴더:" "${DOCKER_ROOT}/backup/$(date +%Y)")
-echo "${rrr}[ ${xxx}${BACKUP_DIR} ${rrr}]${xxx}"
-DB_VOLUME=$(enter_value "${qq}-3. DB 설치위치:" "${DOCKER_ROOT}/pgsql")
-echo "${rrr}[ ${xxx}${DB_VOLUME} ${rrr}]${xxx}"
-DB_NAME=$(enter_value "${qq}-4. pgsql 데이터베이스 이름:" "wikidb")
-echo "${rrr}[ ${xxx}${DB_NAME} ${rrr}]${xxx}"
-DB_USER=$(enter_value "${qq}-5. pgsql 사용자 이름:" "wikiuser")
-echo "${rrr}[ ${xxx}${DB_USER} ${rrr}]${xxx}"
-DB_PASS=$(enter_passwd "${qq}-6. pgsql 비번:" "wikipswd")
+qq=10 #-- 문답 겟수
+
+#-- 두번째 yml 의 초기값을 어레이에 담는다.
+DockerRootDir="/home/docker"
+A_DOCKER_ROOT=("${qq}-1: 도커의 루트 디렉토리:" "${DockerRootDir}")
+A_BACKUP_DIR=("${qq}-2: 백업폴더:" "${DockerRootDir}/backup/$(date +%Y)")
+A_DB_VOLUME=("${qq}-3: DB 설치위치:" "${DockerRootDir}/pgsql")
+A_DB_NAME=("${qq}-4: pgsql 데이터베이스 이름:" "wikidb")
+A_DB_USER=("${qq}-5: pgsql 사용자 이름:" "wikiuser")
+A_DB_PASS=("${qq}-6: pgsql 비번:" "wikipswd")
+A_DB_CONTAINER=("${qq}-7: 데이터베이스 컨테이너 이름:" "dbcon")
+A_WIKI_PORT=("${qq}-8: 위키연결 포트번호:" "5940")
+A_WIKI_CONTAINER=("${qq}-9: 위키 컨테이너 이름:" "wikicon")
+A_CLOUD_NAME=("${qq}-10: rclone 백업용 클라우드 저장소 이름:" "tpn3mi")
+
+#-- 첫번째 yml 의 초기값을 어레이에 담는다.
+DockerRootDir="/home/docker"
+A_DOCKER_ROOT=("${qq}-1. 도커의 루트 디렉토리:" "${DockerRootDir}")
+A_BACKUP_DIR=("${qq}-2. 백업폴더:" "${DockerRootDir}/backup/$(date +%Y)")
+A_DB_VOLUME=("${qq}-3. DB 설치위치:" "${DockerRootDir}/pgsql")
+A_DB_NAME=("${qq}-4. pgsql 데이터베이스 이름:" "wiki")
+A_DB_USER=("${qq}-5. pgsql 사용자 이름:" "wikijs")
+A_DB_PASS=("${qq}-6. pgsql 비번:" "wikijsrocks")
+A_DB_CONTAINER=("${qq}-7. 데이터베이스 컨테이너 이름:" "wikijsdb")
+A_WIKI_PORT=("${qq}-8. 위키연결 포트번호:" "5940")
+A_WIKI_CONTAINER=("${qq}-9. 위키 컨테이너 이름:" "wikijs")
+A_CLOUD_NAME=("${qq}-10. rclone 백업용 클라우드 저장소 이름:" "tpn3mi")
+
+#-- 값 입력
+DOCKER_ROOT=$(enter_value "${A_DOCKER_ROOT[0]}" "${A_DOCKER_ROOT[1]}")
+BACKUP_DIR=$(enter_value "${A_BACKUP_DIR[0]}" "${A_BACKUP_DIR[1]}")
+DB_VOLUME=$(enter_value "${A_DB_VOLUME[0]}" "${A_DB_VOLUME[1]}")
+DB_NAME=$(enter_value "${A_DB_NAME[0]}" "${A_DB_NAME[1]}")
+DB_USER=$(enter_value "${A_DB_USER[0]}" "${A_DB_USER[1]}")
+DB_PASS=$(enter_passwd "${A_DB_PASS[0]}" "${A_DB_PASS[1]}")
 echo "........"
 echo "password"
-###- echo "${rrr}[ ${xxx}${DB_PASS} ${rrr}]${xxx}"
-DB_CONTAINER=$(enter_value "${qq}-7. 데이터베이스 컨테이너 이름:" "dbcon")
-echo "${rrr}[ ${xxx}${DB_CONTAINER} ${rrr}]${xxx}"
-WIKI_PORT=$(enter_value "${qq}-8. 위키연결 포트번호:" "5940")
-echo "${rrr}[ ${xxx}${WIKI_PORT} ${rrr}]${xxx}"
-WIKI_CONTAINER=$(enter_value "${qq}-9. 위키 컨테이너:" "wikicon")
-echo "${rrr}[ ${xxx}${WIKI_CONTAINER} ${rrr}]${xxx}"
-CLOUD_NAME=$(enter_value "${qq}-10. rclone 백업용 클라우드 저장소 이름:" "tpn3mi")
-echo "${rrr}[ ${xxx}${CLOUD_NAME} ${rrr}]${xxx}"
+DB_CONTAINER=$(enter_value "${A_DB_CONTAINER[0]}" "${A_DB_CONTAINER[1]}")
+WIKI_PORT=$(enter_value "${A_WIKI_PORT[0]}" "${A_WIKI_PORT[1]}")
+WIKI_CONTAINER=$(enter_value "${A_WIKI_CONTAINER[0]}" "${A_WIKI_CONTAINER[1]}")
+CLOUD_NAME=$(enter_value "${A_CLOUD_NAME[0]}" "${A_CLOUD_NAME[1]}")
 
 #--
 if [ ! -d ${BACKUP_DIR} ]; then
@@ -118,45 +136,49 @@ if [ "x$N_is_current_no_backup" = 'xn' ]; then
 # |
 # | $(date +%y%m%d_%a-%H%M)
 # |
-# | (5-0) 현재의 wiki DB 를 백업 하지 않습니다.
+# | (5-a) 현재의 wiki DB 를 백업 하지 않습니다.
 # |
 __EOF__
 
 else
-    #-- ----> 현재 wiki DB 를 다운로드 + 백업 합니다.
+	#-- (5-b) ----> 현재 wiki DB 를 다운로드 + 백업 합니다.
     #--
     cat <<__EOF__
 # |
-# | (5-1) 백업하는 .7z 파일에 지정해 줄 새로운 ===${ccc}비밀번호${xxx}=== 를 입력하세요.
+# | (5-b.1) 백업하는 .7z 파일에 지정해 줄 새로운 ===${ccc}비밀번호${xxx}=== 를 입력하세요.
 ${bbb}v=== 비번 입력:${xxx}
 __EOF__
     read -s BACKUP_PSWD
 
     BACKUP_FILE=${BACKUP_DIR}/wiki_$(date +%y%m%d_%a-%H%M)-$(uname -n).sql.7z
 	CLOUD_Y2M2=${DB_CONTAINER}/$(date +%y%m)
-    cmdrun "time sudo bash -c \"docker exec ${DB_CONTAINER} pg_dumpall -U ${DB_USER} | 7za a -mx=9 -p${BACKUP_PSWD} -si ${BACKUP_FILE}\"" "(5-2) 업로드 하기전에, 현재의 wiki DB 파일을 백업합니다."
+    cmdrun "sudo bash -c \"docker exec ${DB_CONTAINER} pg_dumpall -U ${DB_USER} | 7za a -mx=9 -p${BACKUP_PSWD} -si ${BACKUP_FILE}\"" "(5-b.2) 업로드 하기전에, 현재의 wiki DB 파일을 백업합니다."
 
-    cmdrun "time rclone copy ${BACKUP_FILE} ${CLOUD_NAME}:${CLOUD_Y2M2}/" "(5-3) wiki DB 파일을 클라우드로 복사합니다."
-    cmdrun "time rclone lsl ${CLOUD_NAME}:${CLOUD_Y2M2}" "(5-4) 클라우드 폴더의 내용입니다."
+    cmdrun "rclone copy ${BACKUP_FILE} ${CLOUD_NAME}:${CLOUD_Y2M2}/" "(5-b.3) wiki DB 파일을 클라우드로 복사합니다."
+    cmdrun "rclone lsl ${CLOUD_NAME}:${CLOUD_Y2M2}" "(5-b.4) 클라우드 폴더의 내용입니다."
     #--
-    #-- <---- 현재 wiki DB 를 다운로드 + 백업 합니다.
+	#-- (5-b) <---- 현재 wiki DB 를 다운로드 + 백업 합니다.
 fi
 
-cmdrun "time sudo docker exec -it ${DB_CONTAINER} dropdb -U ${DB_USER} ${DB_NAME}" "(6) 현재의 wiki DB 를 삭제합니다."
+echo "sudo docker exec -it ${DB_CONTAINER} dropdb -U ${DB_USER} ${DB_NAME}------(6) 현재의 wiki DB 를 삭제합니다."
+sudo docker exec -it ${DB_CONTAINER} dropdb -U ${DB_USER} ${DB_NAME}
 
-cmdrun "time sudo docker exec -it ${DB_CONTAINER} createdb -U ${DB_USER} ${DB_NAME}" "(7) 빈 wiki DB 를 새로 만듭니다."
+### cmdrun "sudo rm -rf ../pgsql" "(6-2) 현재의 wiki DB 를 담은 폴더도 삭제합니다."
+
+echo "sudo docker exec -it ${DB_CONTAINER} createdb -U ${DB_USER} ${DB_NAME}------(7) 빈 wiki DB 를 새로 만듭니다."
+sudo docker exec -it ${DB_CONTAINER} createdb -U ${DB_USER} ${DB_NAME}
 
 cat <<__EOF__
 # |
-# | (8) 백업된 wiki DB 파일을 데이터베이스에 다시 붓습니다. (RESTORE)
+# | 백업된 wiki DB 파일을 데이터베이스에 다시 붓습니다. (RESTORE)
 # |
-# | (9) 백업할때 입력한 ----비밀번호---- 를 입력하세요.
+# | (8) 백업할때 입력한 ----비밀번호---- 를 입력하세요.
 # |
 ${bbb}vvvv 비번 입력:${xxx}
 __EOF__
 read -s backup_pswd
 
-cmdrun "time 7za x -p${backup_pswd} -so ${RESTORE_FILE} | sudo docker exec -i ${DB_CONTAINER} psql -U ${DB_USER} -d ${DB_NAME}" "(8) 백업된 wiki DB 파일을 데이터베이스에 다시 붓습니다. (RESTORE)"
+cmdrun "7za x -p${backup_pswd} -so ${RESTORE_FILE} | sudo docker exec -i ${DB_CONTAINER} psql -U ${DB_USER} -d ${DB_NAME}" "(9) 백업된 wiki DB 파일을 데이터베이스에 다시 붓습니다. (RESTORE)"
 
 cmdrun "sudo docker ps -a" "(10) 멈췄던 위키 컨테이너를 다시 시작합니다."
 
@@ -182,7 +204,6 @@ ls -l ${RESTORE_FILE}
 NEW_DOCKER_FILE="${DOCKER_ROOT}/compose/docker-compose-$(date +%y%m%d_%a-%H%M).yml"
 
 cat > ${NEW_DOCKER_FILE} <<__EOF__
-version: "3"
 services:
 
   db:
